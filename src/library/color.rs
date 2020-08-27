8a45ec2875a4f72cb02d574b63b5dda141da35c1
Laurenz <laurmaedje@gmail.com>
2020-08-27 21:56:53 +0200

    Fix default values for rgb-clamping 🩹

--- src/library/color.rs
+++ src/library/color.rs
@@ -5,22 +5,27 @@ use super::*;
 pub async fn rgb(span: Span, mut args: TableValue, _: LayoutContext<'_>) -> Pass<Value> {
     let mut f = Feedback::new();
 
+    let r = args.expect::<Spanned<f64>>("red value", span, &mut f);
+    let g = args.expect::<Spanned<f64>>("green value", span, &mut f);
+    let b = args.expect::<Spanned<f64>>("blue value", span, &mut f);
+    let a = args.take::<Spanned<f64>>();
+
+    let mut clamp = |component: Option<Spanned<f64>>, default| {
+        component.map(|c| {
+            if c.v < 0.0 || c.v > 255.0 {
+                error!(@f, c.span, "should be between 0 and 255")
+            }
+            c.v.min(255.0).max(0.0).round() as u8
+        }).unwrap_or(default)
+    };
+
     let color = RgbaColor::new(
-        clamp(args.expect::<Spanned<f64>>("red value", span, &mut f), &mut f),
-        clamp(args.expect::<Spanned<f64>>("green value", span, &mut f), &mut f),
-        clamp(args.expect::<Spanned<f64>>("blue value", span, &mut f), &mut f),
-        clamp(args.take::<Spanned<f64>>(), &mut f),
+        clamp(r, 0),
+        clamp(g, 0),
+        clamp(b, 0),
+        clamp(a, 255),
     );
 
     args.unexpected(&mut f);
     Pass::new(Value::Color(color), f)
-}
-
-fn clamp(component: Option<Spanned<f64>>, f: &mut Feedback) -> u8 {
-    component.map(|c| {
-        if c.v < 0.0 || c.v > 255.0 {
-            error!(@f, c.span, "should be between 0 and 255")
-        }
-        c.v.min(255.0).max(0.0).round() as u8
-    }).unwrap_or_default()
 }
