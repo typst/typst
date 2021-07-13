0481192a77f953e3bef727326bd93413f709c447
Laurenz <laurmaedje@gmail.com>
2021-07-13 15:11:06 +0200

    New shiny benchmarks with iai!

---
+++ bench/src/parsing.rs
@@ -0,0 +1,37 @@
+use iai::{black_box, main};
+
+use typst::diag::Pass;
+use typst::parse::{parse, Scanner, TokenMode, Tokens};
+use typst::syntax::SyntaxTree;
+
+const SRC: &str = include_str!("../../tests/typ/coma.typ");
+
+fn bench_decode() -> usize {
+    // We don't use chars().count() because that has a special
+    // superfast implementation.
+    let mut count = 0;
+    let mut chars = black_box(SRC).chars();
+    while let Some(_) = chars.next() {
+        count += 1;
+    }
+    count
+}
+
+fn bench_scan() -> usize {
+    let mut count = 0;
+    let mut scanner = Scanner::new(black_box(SRC));
+    while let Some(_) = scanner.eat() {
+        count += 1;
+    }
+    count
+}
+
+fn bench_tokenize() -> usize {
+    Tokens::new(black_box(SRC), black_box(TokenMode::Markup)).count()
+}
+
+fn bench_parse() -> Pass<SyntaxTree> {
+    parse(black_box(SRC))
+}
+
+main!(bench_decode, bench_scan, bench_tokenize, bench_parse);
