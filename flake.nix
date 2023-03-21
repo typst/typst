e46ae61c1f32dc3b77693c40bec58e38d6d1977c
Evan Richter <evanjrichter@gmail.com>
2023-03-21 15:38:07 -0500

    nix flake dev environment

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , rust-overlay
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain =
+            let
+              rust = super.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default;
+        })
+      ];
+
+      pkgs = import nixpkgs { inherit system overlays; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+        ] ++ lib.optionals stdenv.isDarwin [
+          pkgs.darwin.apple_sdk.frameworks.CoreServices
+        ];
+
+        shellHook = ''
+          ${pkgs.rustToolchain}/bin/cargo --version
+        '';
+      };
+    });
+}
