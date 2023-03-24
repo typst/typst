3724b0d87771e6c8abe8630c9c105fe4d1afeef2
Greg Shuflin <greg@everydayimshuflin.com>
2023-03-23 20:59:06 -0700

    Improve error message in lexer
    In the error message for an invalid character, print which character
    triggered the error.

--- src/syntax/lexer.rs
+++ src/syntax/lexer.rs
@@ -489,7 +489,7 @@ impl Lexer<'_> {
 
             c if is_id_start(c) => self.ident(start),
 
-            _ => self.error("this character is not valid in code"),
+            c => self.error(format!("The character `{c}` is not valid in code")),
         }
     }
 
