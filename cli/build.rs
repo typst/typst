e70ec5f3c06312b7ff2388630e05e3c2d745896f
figsoda <figsoda@pm.me>
2023-03-25 10:38:34 -0400

    Don't let the build script overwrite TYPST_VERSION (#154)

--- cli/build.rs
+++ cli/build.rs
@@ -1,11 +1,16 @@
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=TYPST_VERSION");
+    if option_env!("TYPST_VERSION").is_some() {
+        return;
+    }
+
     let version = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .ok()
-        .and_then(|output| output.status.success().then(|| output))
+        .filter(|output| output.status.success())
         .and_then(|output| String::from_utf8(output.stdout.get(..8)?.into()).ok())
         .unwrap_or_else(|| "(unknown version)".into());
     println!("cargo:rustc-env=TYPST_VERSION={version}");
