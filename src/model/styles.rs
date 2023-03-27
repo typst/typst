4d52e7fda07b256353f6656af606808ec41a61f2
Marek Barvíř <barvirm@gmail.com>
2023-03-28 00:19:02 +0200

    Update styles.rs

--- src/model/styles.rs
+++ src/model/styles.rs
@@ -433,7 +433,7 @@ impl<'a> StyleChain<'a> {
             values
                 .next()
                 .map(|value| value.fold(next(values, styles, default)))
-                .unwrap_or(default())
+                .unwrap_or_default()
         }
         next(self.properties::<T>(func, name, inherent), self, &default)
     }
