9737d3b754c871d7ca9c63790b5d4cefda07d6ff
Marco Radocchia <marco.radocchia@outlook.com>
2023-03-29 19:34:18 +0200

    Added IT language support (#413)

--- library/src/meta/outline.rs
+++ library/src/meta/outline.rs
@@ -180,6 +180,7 @@ impl LocalName for OutlineElem {
     fn local_name(&self, lang: Lang) -> &'static str {
         match lang {
             Lang::GERMAN => "Inhaltsverzeichnis",
+            Lang::ITALIAN => "Indice",
             Lang::ENGLISH | _ => "Contents",
         }
     }
