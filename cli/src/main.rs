b77df8833ad038d321c4189e684934932e0062d5
Johannes Wolf <git@johannes-wolf.com>
2023-03-29 03:04:54 +0200

    cli: Allow reading every path not of type dir

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -563,10 +563,10 @@ impl PathHash {
 /// Read a file.
 fn read(path: &Path) -> FileResult<Vec<u8>> {
     let f = |e| FileError::from_io(e, path);
-    if fs::metadata(&path).map_err(f)?.is_file() {
-        fs::read(&path).map_err(f)
-    } else {
+    if fs::metadata(&path).map_err(f)?.is_dir() {
         Err(FileError::IsDirectory)
+    } else {
+        fs::read(&path).map_err(f)
     }
 }
 
