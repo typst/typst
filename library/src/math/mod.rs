c53b182045944ed869f1c25315338565ff1f5204
onebitbool <onebitbool@gmail.com>
2023-03-31 20:19:47 +0800

    Added ZH language support

--- library/src/math/mod.rs
+++ library/src/math/mod.rs
@@ -267,6 +267,7 @@ impl Count for EquationElem {
 impl LocalName for EquationElem {
     fn local_name(&self, lang: Lang) -> &'static str {
         match lang {
+            Lang::CHINESE => "等式",
             Lang::GERMAN => "Gleichung",
             Lang::ITALIAN => "Equazione",
             Lang::RUSSIAN => "Уравнение",
