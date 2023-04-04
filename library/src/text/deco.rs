591e5413804f5e5897d651b8850df266ba585b1b
Marek Barvíř <barvirm@gmail.com>
2023-04-04 21:52:31 +0200

    bind expresion to variable

--- library/src/text/deco.rs
+++ library/src/text/deco.rs
@@ -314,11 +314,13 @@ pub(super) fn decorate(
 
         // Only do the costly segments intersection test if the line
         // intersects the bounding box.
-        if bbox.map_or(false, |bbox| {
+        let intersect = bbox.map_or(false, |bbox| {
             let y_min = -text.font.to_em(bbox.y_max).at(text.size);
             let y_max = -text.font.to_em(bbox.y_min).at(text.size);
             offset >= y_min && offset <= y_max
-        }) {
+        });
+
+        if intersect {
             // Find all intersections of segments with the line.
             intersections.extend(
                 path.segments()
