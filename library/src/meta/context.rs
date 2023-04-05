595044344dd4db6d145c11186a53026599468809
Sébastien d'Herbais de Thun <sebastien.d.herbais@gmail.com>
2023-04-05 14:54:45 +0200

    Added doc for `page-numbering`

--- library/src/meta/context.rs
+++ library/src/meta/context.rs
@@ -34,6 +34,15 @@ use crate::prelude::*;
 /// to skip unnecessary work.
 ///
 /// - returns: dictionary
+/// 
+/// ### page-numbering()
+/// Returns the page numbering pattern of the page at this location. This can be
+/// used when displaying the page counter in order to obtain the local numbering.
+/// This is useful if you are building custom indices or outlines.
+/// 
+/// If the page numbering is set to `none` at that location, this function returns `none`.
+/// 
+/// - returns: numbering or none
 ///
 /// Display: Locate
 /// Category: meta
