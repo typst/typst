d15726cc69d8fdf4d3af15a1e61cab564d60bfcd
marmare314 <49279081+Marmare314@users.noreply.github.com>
2023-04-07 07:11:12 +0200

    minor style improvements

--- src/eval/func.rs
+++ src/eval/func.rs
@@ -313,8 +313,8 @@ impl Closure {
         // Parse the arguments according to the parameter list.
         let num_pos_params =
             closure.params.iter().filter(|p| matches!(p, Param::Pos(_))).count();
-        let num_pos_args = args.to_pos().len();
-        let sink_size = (num_pos_args as usize).checked_sub(num_pos_params);
+        let num_pos_args = args.to_pos().len() as usize;
+        let sink_size = num_pos_args.checked_sub(num_pos_params);
 
         let mut sink = None;
         let mut sink_pos_values = None;
@@ -324,7 +324,7 @@ impl Closure {
                     vm.define(ident.clone(), args.expect::<Value>(ident)?);
                 }
                 Param::Sink(ident) => {
-                    sink = Some(ident.clone());
+                    sink = ident.clone();
                     if let Some(sink_size) = sink_size {
                         if let Some(remaining_args) = args.consume(sink_size) {
                             sink_pos_values = Some(remaining_args);
@@ -341,7 +341,7 @@ impl Closure {
             }
         }
 
-        if let Some(Some(sink)) = sink {
+        if let Some(sink) = sink {
             let mut remaining_args = args.take();
             if let Some(sink_pos_values) = sink_pos_values {
                 remaining_args.items.extend(sink_pos_values);
