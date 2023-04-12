db3d9e53d38c5df41aeefcee024f8c8b476ef572
Alex Saveau <saveau.alexandre@gmail.com>
2023-04-11 21:15:17 -0700

    Convert math alignment to single-pass algorithm
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- library/src/math/align.rs
+++ library/src/math/align.rs
@@ -16,33 +16,28 @@ impl LayoutMath for AlignPointElem {
 
 /// Determine the position of the alignment points.
 pub(super) fn alignments(rows: &[MathRow]) -> Vec<Abs> {
-    let count = rows
-        .iter()
-        .map(|row| {
-            row.iter()
-                .filter(|fragment| matches!(fragment, MathFragment::Align))
-                .count()
-        })
-        .max()
-        .unwrap_or(0);
+    let mut points = Vec::<Abs>::new();
 
-    let mut points = vec![Abs::zero(); count];
-    for current in 0..count {
-        for row in rows {
-            let mut x = Abs::zero();
-            let mut i = 0;
-            for fragment in row.iter() {
-                if matches!(fragment, MathFragment::Align) {
-                    if i < current {
-                        x = points[i];
-                    } else if i == current {
-                        points[i].set_max(x);
-                    }
-                    i += 1;
+    for row in rows {
+        let mut width = Abs::zero();
+        let mut alignment_index = 0;
+        for fragment in row.iter() {
+            if matches!(fragment, MathFragment::Align) {
+                if alignment_index < points.len() {
+                    points[alignment_index].set_max(width);
+                } else {
+                    points.push(width);
                 }
-                x += fragment.width();
+                width = Abs::zero();
+                alignment_index += 1;
+            } else {
+                width += fragment.width();
             }
         }
+    }
+    for i in 1..points.len() {
+        let prev = points[i - 1];
+        points[i] += prev;
     }
 
     points
