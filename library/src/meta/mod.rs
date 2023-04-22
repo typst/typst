5f8c1ec6ac47f308176f3205ed177717441db773
pan93412 <pan93412@gmail.com>
2023-04-22 09:35:55 +0800

    feat!(meta): Pass `region` to `local_name`

--- library/src/meta/mod.rs
+++ library/src/meta/mod.rs
@@ -28,8 +28,11 @@ pub use self::state::*;
 
 use typst::doc::Lang;
 
+pub type Region<'a> = &'a str;
+pub type OptRegion<'a> = Option<Region<'a>>;
+
 /// The named with which an element is referenced.
 pub trait LocalName {
-    /// Get the name in the given language.
-    fn local_name(&self, lang: Lang) -> &'static str;
+    /// Get the name in the given language and (optionally) region.
+    fn local_name(&self, lang: Lang, region: OptRegion<'_>) -> &'static str;
 }
