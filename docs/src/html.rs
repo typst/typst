e6a7c4dbadfc40dbf0a9a26939c651058c36765b
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2023-04-28 21:46:47 +0200

    Fixed missing today implementation

--- docs/src/html.rs
+++ docs/src/html.rs
@@ -384,7 +384,7 @@ impl World for DocWorld {
             .into())
     }
 
-    fn now(&self, _: bool) -> (i32, u8, u8, u8, u8, u8) {
-        (1970, 1, 1, 0, 0, 0)
+    fn today(&self, _: bool) -> Option<(i32, u8, u8)> {
+        Some((1970, 1, 1))
     }
 }
