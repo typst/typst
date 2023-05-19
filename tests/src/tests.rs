418f35a05b8612070bb650eaefcf7baa25a4a82a
Eric Biedert <github@ericbiedert.de>
2023-05-19 18:18:32 +0200

    Strip '\r' suffix only once

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -375,7 +375,11 @@ fn test(
     let mut compare_ever = false;
     let mut rng = LinearShift::new();
 
-    let parts: Vec<_> = text.split("\n---").map(|s| s.trim_end_matches('\r')).collect();
+    let parts: Vec<_> = text
+        .split("\n---")
+        .map(|s| s.strip_suffix('\r').unwrap_or(s))
+        .collect();
+
     for (i, &part) in parts.iter().enumerate() {
         if let Some(x) = args.subtest {
             let x = usize::try_from(
