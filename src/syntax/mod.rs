168bdf35bd773e67343c965cb473492cc5cae9e7
Laurenz <laurmaedje@gmail.com>
2023-06-06 19:10:16 +0200

    Add note to `groups.yml`

--- src/syntax/mod.rs
+++ src/syntax/mod.rs
@@ -10,9 +10,14 @@ mod reparser;
 mod source;
 mod span;
 
-pub use self::kind::*;
-pub use self::lexer::*;
-pub use self::node::*;
-pub use self::parser::*;
-pub use self::source::*;
-pub use self::span::*;
+pub use self::kind::SyntaxKind;
+pub use self::lexer::{is_ident, is_newline};
+pub use self::node::{ErrorPos, LinkedChildren, LinkedNode, SyntaxNode};
+pub use self::parser::{parse, parse_code};
+pub use self::source::{Source, SourceId};
+pub use self::span::{Span, Spanned};
+
+pub(crate) use self::lexer::{is_id_continue, is_id_start};
+
+use self::lexer::{split_newlines, LexMode, Lexer};
+use self::parser::{reparse_block, reparse_markup};
