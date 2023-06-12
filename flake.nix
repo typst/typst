638a534273b712fd8a3fe37632ae88a2fe75aa72
figsoda <figsoda@pm.me>
2023-06-12 07:49:39 -0400

    Fix Nix flake (#1450)
    Typst now requires Rust 1.70 which is not available in nixpkgs yet, so we switch to fenix instead

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, fenix, nixpkgs }:
     let
       inherit (nixpkgs.lib)
         genAttrs
@@ -24,53 +28,65 @@
       rev = fallback:
         self.shortRev or fallback;
 
-      packageFor = pkgs: pkgs.rustPlatform.buildRustPackage rec {
-        pname = "typst";
-        inherit ((importTOML ./Cargo.toml).workspace.package) version;
+      packageFor = pkgs:
+        let
+          rust = fenix.packages.${pkgs.stdenv.hostPlatform.system}.minimal.toolchain;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust;
+            rustc = rust;
+          };
+        in
+        rustPlatform.buildRustPackage rec {
+          pname = "typst";
+          inherit ((importTOML ./Cargo.toml).workspace.package) version;
 
-        src = cleanSource ./.;
+          src = cleanSource ./.;
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-          allowBuiltinFetchGit = true;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            allowBuiltinFetchGit = true;
+          };
+
+          nativeBuildInputs = [
+            pkgs.installShellFiles
+          ];
+
+          buildInputs = optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.CoreServices
+          ];
+
+          postInstall = ''
+            installManPage cli/artifacts/*.1
+            installShellCompletion \
+              cli/artifacts/typst.{bash,fish} \
+              --zsh cli/artifacts/_typst
+          '';
+
+          GEN_ARTIFACTS = "artifacts";
+          TYPST_VERSION = "${version} (${rev "unknown hash"})";
         };
-
-        nativeBuildInputs = [
-          pkgs.installShellFiles
-        ];
-
-        buildInputs = optionals pkgs.stdenv.isDarwin [
-          pkgs.darwin.apple_sdk.frameworks.CoreServices
-        ];
-
-        postInstall = ''
-          installManPage cli/artifacts/*.1
-          installShellCompletion \
-            cli/artifacts/typst.{bash,fish} \
-            --zsh cli/artifacts/_typst
-        '';
-
-        GEN_ARTIFACTS = "artifacts";
-        TYPST_VERSION = "${version} (${rev "unknown hash"})";
-      };
     in
     {
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            clippy
-            rust-analyzer
-            rustc
-            rustfmt
-          ];
+          packages =
+            let
+              fenix' = fenix.packages.${pkgs.stdenv.hostPlatform.system};
+            in
+            [
+              (fenix'.default.withComponents [
+                "cargo"
+                "clippy"
+                "rustc"
+                "rustfmt"
+              ])
+              fenix'.rust-analyzer
+            ];
 
           buildInputs = optionals pkgs.stdenv.isDarwin [
             pkgs.darwin.apple_sdk.frameworks.CoreServices
             pkgs.libiconv
           ];
-
-          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         };
       });
 
