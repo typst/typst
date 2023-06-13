09e92632e018af317bf5bb3f02f05abfe60382ff
raphCode <15750438+raphCode@users.noreply.github.com>
2023-06-13 13:54:40 +0200

    Add note that show rules might not apply to footnotes

--- library/src/meta/footnote.rs
+++ library/src/meta/footnote.rs
@@ -27,6 +27,10 @@ use crate::visualize::LineElem;
 /// The footnote automatically attaches itself to the preceding word, even if
 /// there is a space before it in the markup. To force space, you can use the
 /// string `[#" "]` or explicit [horizontal spacing]($func/h).
+/// 
+/// Please note that show rules in scope where `#footnote` is called may not
+/// apply to the footnote's content in some cases. For more information see:
+/// https://github.com/typst/typst/issues/1467#issuecomment-1588799440
 ///
 /// Display: Footnote
 /// Category: meta
