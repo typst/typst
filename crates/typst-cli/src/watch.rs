4b79597b63dec9f5f7b7c72446a9114cd83d8b42
Trévis Morvany <trevismorvany@gmail.com>
2023-07-09 01:10:09 +0200

    Added a comment explaining why the fix is needed

--- crates/typst-cli/src/watch.rs
+++ crates/typst-cli/src/watch.rs
@@ -43,6 +43,10 @@ pub fn watch(mut command: CompileCommand) -> StrResult<()> {
         {
             let event = event.map_err(|_| "failed to watch directory")?;
 
+            // Workaround for notify-rs' implicit unwatch on remove/rename (triggered
+            // by some editors when saving files) with the inotify backend.
+            // By keeping track of the removed files, we can allow those we still
+            // depend on to be watched again later on.
             if matches!(
                 event.kind,
                 notify::EventKind::Remove(notify::event::RemoveKind::File)
