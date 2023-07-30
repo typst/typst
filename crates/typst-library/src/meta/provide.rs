b4fce0eb084a707c5925a6e263563dedf049ca3b
Rico Beier-Grunwald <beier1@hs-mittweida.de>
2023-07-30 20:40:29 +0200

    short comment for provide

--- crates/typst-library/src/meta/provide.rs
+++ crates/typst-library/src/meta/provide.rs
@@ -1,6 +1,25 @@
 use crate::prelude::*;
 
-/// Todo: TODO!
+/// Provide custom metadata for the Typst query system.
+///
+/// The Typst query system allows users to extract metadata from the document, offering both a
+/// generic selector and a specific key-value mechanism. The 'provide' command is an essential
+/// part of this mechanism, associating a single piece of metadata with the specified key.
+/// Subsequent invocations will append to a list of values for the same key.
+///
+/// Example:
+/// ```example
+/// #provide("note", "This is a note")
+///
+/// While this note has no visible output in the document, it embeds metadata into the document! This metadata can be retrieved using the 'query' command.
+/// ```
+///
+/// How to retrieve the metadata:
+/// ```sh
+/// $ typst query example.typ --key note
+/// ["This is a note"]
+/// ```
+///
 /// Display: Provide
 /// Category: meta
 #[element(Behave, Show, Locatable)]
