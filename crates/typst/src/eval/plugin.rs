3eeaec2006df4183a5a4932da9feee1214df762f
astrale-sharp <ash4567@outlook.fr>
2023-08-06 14:39:37 +0000

    no copy protocol update updates hello.wasm to new protocol version

--- crates/typst/src/eval/plugin.rs
+++ crates/typst/src/eval/plugin.rs
@@ -1,11 +1,10 @@
-use super::{cast, Args, Value};
-use crate::diag::SourceResult;
-use crate::diag::StrResult;
-use crate::Bytes;
+use super::{cast, Args, Bytes, Value};
+use crate::diag::{SourceResult, StrResult};
+use ecow::EcoString;
 use std::sync::{Arc, Mutex, MutexGuard};
 use typst::diag::At;
 use wasmi::{
-    AsContext, AsContextMut, Caller, Engine, Func as Function, Linker, Module,
+    AsContext, AsContextMut, Caller, Engine, Func as Function, Linker, Memory, Module,
     Value as WasiValue,
 };
 
@@ -20,6 +19,8 @@ struct Repr {
     bytes: Bytes,
     functions: Vec<(String, Function)>,
     store: Mutex<Store>,
+    memory: Memory,
+    free_function: Function,
 }
 
 cast! {
@@ -42,15 +43,61 @@ type Store = wasmi::Store<PersistentData>;
 
 #[derive(Debug, Clone)]
 struct PersistentData {
-    result_data: Vec<u8>,
+    result_ptr: u32,
+    result_len: u32,
     arg_buffer: Vec<u8>,
+}
+
+/// Reference to a slice of memory returned after
+/// [calling a wasm function](PluginInstance::call).
+///
+/// # Drop
+/// On [`Drop`], this will free the slice of memory inside the plugin.
+///
+/// As such, this structure mutably borrows the [`PluginInstance`], which prevents
+/// another function from being called.
+struct ReturnedData<'a> {
+    memory: Memory,
+    ptr: u32,
+    len: u32,
+    free_function: &'a Function,
+    context_mut: MutexGuard<'a, Store>,
+}
+
+impl<'a> ReturnedData<'a> {
+    /// Get a reference to the returned slice of data.
+    ///
+    /// # Panic
+    /// This may panic if the function returned an invalid `(ptr, len)` pair.
+    pub fn get(&self) -> &[u8] {
+        &self.memory.data(&*self.context_mut)
+            [self.ptr as usize..(self.ptr + self.len) as usize]
+    }
+}
+
+impl Drop for ReturnedData<'_> {
+    fn drop(&mut self) {
+        if self.ptr != 0 {
+            self.free_function
+                .call(
+                    &mut *self.context_mut,
+                    &[WasiValue::I32(self.ptr as _), WasiValue::I32(self.len as _)],
+                    &mut [],
+                )
+                .unwrap();
+        }
+    }
 }
 
 impl Plugin {
     /// creates a new plugin.
     pub fn new_from_bytes(bytes: impl AsRef<[u8]>) -> StrResult<Self> {
         let engine = Engine::default();
-        let data = PersistentData { result_data: Vec::new(), arg_buffer: Vec::new() };
+        let data = PersistentData {
+            arg_buffer: Vec::new(),
+            result_ptr: 0,
+            result_len: 0,
+        };
         let mut store = Store::new(&engine, data);
 
         let module = Module::new(&engine, bytes.as_ref())
@@ -62,12 +109,8 @@ impl Plugin {
                 "typst_env",
                 "wasm_minimal_protocol_send_result_to_host",
                 move |mut caller: Caller<PersistentData>, ptr: u32, len: u32| {
-                    let memory =
-                        caller.get_export("memory").unwrap().into_memory().unwrap();
-                    let mut buffer = std::mem::take(&mut caller.data_mut().result_data);
-                    buffer.resize(len as usize, 0);
-                    memory.read(&caller, ptr as _, &mut buffer).unwrap();
-                    caller.data_mut().result_data = buffer;
+                    caller.data_mut().result_ptr = ptr;
+                    caller.data_mut().result_len = len;
                 },
             )
             .unwrap()
@@ -88,17 +131,29 @@ impl Plugin {
             .start(&mut store)
             .map_err(|e| format!("{e}"))?;
 
+        let mut free_function = None;
         let functions = instance
             .exports(&store)
             .filter_map(|e| {
                 let name = e.name().to_owned();
-                e.into_func().map(|func| (name, func))
+
+                e.into_func().map(|func| {
+                    if name == "wasm_minimal_protocol_free_byte_buffer" {
+                        free_function = Some(func);
+                    }
+                    (name, func)
+                })
             })
             .collect::<Vec<_>>();
+        let free_function = free_function.ok_or(EcoString::from("Module didn't export a free function."))?;
+        let memory =
+            instance.get_export(&store, "memory").unwrap().into_memory().unwrap();
         Ok(Plugin(Arc::new(Repr {
-            bytes: bytes.as_ref().into(),
-            functions,
             store: Mutex::new(store),
+            memory,
+            free_function,
+            functions,
+            bytes: bytes.as_ref().into(),
         })))
     }
 
@@ -132,43 +187,67 @@ impl Plugin {
             byte_args.push(arg);
         }
         let byte_args = byte_args.iter().map(|b| b.as_slice()).collect::<Vec<_>>();
-        let s = self.call_inner(function_name, &byte_args).at(span)?;
-        Ok(Value::Bytes(s.into()))
+        let s = self.call_inner(function_name, byte_args).at(span)?;
+        Ok(Value::Bytes(s.get().into()))
     }
 
-    fn call_inner(&self, function: &str, args: &[&[u8]]) -> Result<Vec<u8>, String> {
-        self.store().data_mut().arg_buffer = args.concat();
+    fn call_inner<'a>(
+        &self,
+        function: &str,
+        args: impl IntoIterator<Item = &'a [u8]>,
+    ) -> StrResult<ReturnedData> {
+        self.store().data_mut().result_ptr = 0;
+        self.store().data_mut().result_len = 0;
 
-        let (_, function) = self
-            .0
-            .functions
-            .iter()
-            .find(|(s, _)| s == function)
-            .ok_or(format!("Plugin doesn't have the method: {function}"))?;
+        let mut result_args = Vec::new();
+        let mut guard = self.store();
+        let arg_buffer = &mut guard.data_mut().arg_buffer;
+        arg_buffer.clear();
+        for arg in args {
+            result_args.push(WasiValue::I32(arg.len() as _));
+            arg_buffer.extend_from_slice(arg);
+        }
+        drop(guard);
 
-        let result_args =
-            args.iter().map(|a| WasiValue::I32(a.len() as _)).collect::<Vec<_>>();
+        let function = self
+            .get_function(function)
+            .ok_or(format!("plugin doesn't have the method: {function}"))?;
 
-        let mut code = [WasiValue::I32(2)];
-        let is_err = function
-            .call(&mut self.store().as_context_mut(), &result_args, &mut code)
-            .is_err();
-        let code = if is_err {
-            WasiValue::I32(2)
-        } else {
-            code.first().cloned().unwrap_or(WasiValue::I32(3)) // if the function returns nothing
+        let mut code = WasiValue::I32(2);
+        let ty = function.ty(&self.store().as_context());
+        if ty.params().len() != result_args.len() {
+            return Err("incorrect number of arguments".into());
+        }
+
+        let call_result = function.call(
+            &mut self.store().as_context_mut(),
+            &result_args,
+            std::array::from_mut(&mut code),
+        );
+        let (ptr, len) = (self.store().data().result_ptr, self.store().data().result_len);
+        let result = ReturnedData {
+            memory: self.0.memory,
+            ptr,
+            len,
+            free_function: &self.0.free_function,
+            context_mut: self.store(),
         };
 
-        let s = std::mem::take(&mut self.store().data_mut().result_data);
+        match call_result {
+            Ok(()) => {}
+            Err(wasmi::Error::Trap(_)) => return Err("plugin panicked".into()),
+            Err(_) => return Err("plugin did not respect the protocol".into()),
+        };
 
         match code {
-            WasiValue::I32(0) => Ok(s),
-            WasiValue::I32(1) => Err(match String::from_utf8(s) {
-                Ok(err) => format!("plugin errored with: '{}'", err,),
-                Err(_) => String::from("plugin errored and did not return valid UTF-8"),
+            WasiValue::I32(0) => Ok(result),
+            WasiValue::I32(1) => Err(match std::str::from_utf8(result.get()) {
+                Ok(err) => format!("plugin errored with: '{}'", err,).into(),
+                Err(_) => {
+                    EcoString::from("plugin errored and did not return valid UTF-8")
+                }
             }),
-            WasiValue::I32(2) => Err("plugin panicked".to_string()),
-            _ => Err("plugin did not respect the protocol".to_string()),
+            _ => Err("plugin did not respect the protocol".into()),
         }
     }
 
--- docs/guides/guide-for-writing-plugins.md
+++ docs/guides/guide-for-writing-plugins.md
@@ -49,23 +49,33 @@ Valid plugins need to import two functions (that will be provided by the runtime
 
   Write the arguments for the current function into the buffer pointed at by `ptr`.
 
-  Each function for the protocol receives lengths as its arguments (see [Exported functions](#exported-functions)). The capacity of the buffer pointed at by `ptr` should be at least the sum of all those lengths.
+  Each function for the protocol receives lengths as its arguments (see [User-defined functions](#user-defined-functions)). The capacity of the buffer pointed at by `ptr` should be at least the sum of all those lengths.
 
 - `(import "typst_env" "wasm_minimal_protocol_send_result_to_host" (func (param i32 i32)))`
 
-  The first parameter is a pointer to a buffer (`ptr`), while the second is the length of the buffer (`len`).
+  The first parameter is a pointer to a buffer (`ptr`), the second is the length of the buffer (`len`).
 
-  Reads `len` bytes pointed at by `ptr` into host memory. The memory pointed at by `ptr` can be freed immediately after this function returns.
+  Send `len` and `ptr` to host memory. The buffer must not be freed by the end of the function: it will be freed by the runtime by calling [`wasm_minimal_protocol_send_result_to_host`](#exports).
 
-  If the message should be interpreted as an error message (see [Exported functions](#exported-functions)), it should be encoded as UTF-8.
+  If the message should be interpreted as an error message (see [User-defined functions](#user-defined-functions)), it should be encoded as UTF-8.
 
-## Exported functions
+### Note
+
+If [`wasm_minimal_protocol_send_result_to_host`](#exports) calls `free` (or a similar routine), be careful that the buffer does not point to static memory.
+
+## Exports
+
+Valid plugins need to export a function named `wasm_minimal_protocol_send_result_to_host`, that has signature `func (param i32 i32)`.
+
+This function will be used by the runtime to free the block of memory returned by a [user-defined](#user-defined-functions) function.
+
+## User-defined functions
 
 To conform to the protocol, an exported function should:
 
-- Take `n` arguments `a₁`, `a₂`, ..., `aₙ` of type `u32` (interpreted as lengths, so `usize/size_t` may be preferable), and return one `i32`.
+- Take `n` arguments `a₁`, `a₂`, ..., `aₙ` of type `u32` (interpreted as lengths, so `usize/size_t` may be preferable), and return one `i32`. We will call the return `return_code`.
 - The function should first allocate a buffer `buf` of length `a₁ + a₂ + ⋯ + aₙ`, and call `wasm_minimal_protocol_write_args_to_buffer(buf.ptr)`.
 - The `a₁` first bytes of the buffer constitute the first argument, the `a₂` next bytes the second argument, and so on.
 - Before returning, the function should call `wasm_minimal_protocol_send_result_to_host` to send its result back to the host.
-- To signal success, the function should return `0`.
-- To signal an error, the function should return `1`. The written buffer is then interpreted as an error message.
+- To signal success, `return_code` must be `0`.
+- To signal an error, `return_code` must be `1`. The sent buffer is then interpreted as an error message, and must be encoded as UTF-8.
