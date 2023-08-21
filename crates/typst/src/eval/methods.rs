046029b1e21324c3b10b03423d9bd297b4eafe8f
Simon Rask <33556894+SimonRask@users.noreply.github.com>
2023-08-21 16:26:05 +0200

    Add hint for missing method error for dictionary where a field with a function is present (#1922)

--- crates/typst/src/eval/methods.rs
+++ crates/typst/src/eval/methods.rs
@@ -173,7 +173,17 @@ pub fn call(
             "keys" => dict.keys().into_value(),
             "values" => dict.values().into_value(),
             "pairs" => dict.pairs().into_value(),
-            _ => return missing(),
+            _ => {
+                return if matches!(dict.at(method, None), Ok(Value::Func(_))) {
+                    Err(missing_method(name, method))
+                        .hint(eco_format!(
+                            "to call the function stored in the dictionary, surround the field access with parentheses"
+                        ))
+                        .at(span)
+                } else {
+                    missing()
+                }
+            }
         },
 
         Value::Func(func) => match method {
