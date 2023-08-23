b5cb11fbf8b85bc7bac3125da11a67d05ea56944
jimvdl <jimvdlind@gmail.com>
2023-08-23 20:00:09 +0200

    Apply review suggestions

--- crates/typst-cli/src/update.rs
+++ crates/typst-cli/src/update.rs
@@ -1,10 +1,12 @@
 use std::env;
 use std::fs;
-use std::io::{self, BufReader, Cursor, Read, Seek};
-use std::path::{Path, PathBuf};
+use std::io::Write;
+use std::io::{Cursor, Read};
+use std::path::PathBuf;
 
 use semver::Version;
 use serde::Deserialize;
+use tempfile::NamedTempFile;
 use typst::{diag::bail, diag::StrResult, eval::eco_format};
 use xz2::bufread::XzDecoder;
 use zip::ZipArchive;
@@ -27,19 +29,18 @@ pub fn update(command: UpdateCommand) -> StrResult<()> {
             eprintln!(
                 "Versions older than 0.8 will not have the update command available"
             );
-            eprintln!("Forcing a downgrade might break your installation");
         }
 
         if !command.force && version < &current_tag {
-            eprintln!(
+            bail!(
                 "Downgrading requires the --force flag: `typst update <VERSION> --force`"
             );
-            bail!("update failed");
         }
     }
 
-    let current_exe = env::current_exe()
-        .map_err(|err| eco_format!("failed to locate current exe path: {}", err))?;
+    let current_exe = env::current_exe().map_err(|err| {
+        eco_format!("failed to locate path of the running executable: {err}")
+    })?;
 
     let backup_path = backup_path()?;
 
@@ -53,40 +54,29 @@ pub fn update(command: UpdateCommand) -> StrResult<()> {
             .map_err(|err| eco_format!("unable to revert to backup: {err}"));
     }
 
-    let buffer = Release::from_tag(command.version)
-        .and_then(|release| {
-            if !update_needed(&release)? && !command.force {
-                bail!("already on the latest version");
-            }
-
-            Ok(release)
-        })
-        .and_then(|release| release.download_asset(needed_asset()?))
-        .and_then(|binary| binary.unpack())?;
-
     fs::copy(&current_exe, &backup_path)
         .map_err(|err| eco_format!("backing up failed: {}", err))?;
 
-    let temp_exe = current_exe
-        .parent()
-        .unwrap_or(Path::new("."))
-        .join("typst_update.part");
-    let mut binary_part = fs::File::create(&temp_exe)
-        .map_err(|err| eco_format!("failed to create typst_update.part: {}", err))?;
-    io::copy(&mut buffer.as_slice(), &mut binary_part).map_err(|err| {
+    let release = Release::from_tag(command.version)?;
+
+    if !update_needed(&release)? && !command.force {
+        bail!("already on the latest version");
+    }
+
+    let binary_data = release.download_binary(needed_asset()?)?;
+
+    let mut temp_exe = NamedTempFile::new()
+        .map_err(|err| eco_format!("failed to create temporary binary: {err}"))?;
+    temp_exe
+        .write_all(&binary_data)
+        .map_err(|err| eco_format!("failed to write binary data: {err}"))?;
+
+    self_replace::self_replace(&temp_exe).map_err(|err| {
         fs::remove_file(&temp_exe).ok();
-        eco_format!("failed to write typst_update.part: {}", err)
+        eco_format!("self replace failed: {}", err)
     })?;
 
-    self_replace::self_replace(&temp_exe)
-        .map_err(|err| {
-            fs::remove_file(&temp_exe).ok();
-            eco_format!("self replace failed: {}", err)
-        })
-        .and_then(|_| {
-            fs::remove_file(&temp_exe)
-                .map_err(|err| eco_format!("failed to delete typst_update.part: {}", err))
-        })
+    Ok(())
 }
 
 /// Assets belonging to a GitHub release.
@@ -137,7 +127,7 @@ impl Release {
 
     /// Sorts through the assets from a given [`Release`] and picks the right one
     /// for the target platform, returning its packed binary.
-    pub fn download_asset(&self, asset_name: &str) -> StrResult<PackedBinary> {
+    pub fn download_binary(&self, asset_name: &str) -> StrResult<Vec<u8>> {
         let asset = self
             .assets
             .iter()
@@ -152,80 +142,54 @@ impl Release {
             Err(_) => bail!("failed to load asset (network failed)"),
         };
 
-        let mut buffer = response
-            .header("Content-Length")
-            .and_then(|header| header.parse().ok())
-            .map_or_else(Vec::new, Vec::with_capacity);
+        let mut data = Vec::new();
         response
             .into_reader()
-            .read_to_end(&mut buffer)
+            .read_to_end(&mut data)
             .map_err(|err| eco_format!("failed to read response buffer: {err}"))?;
 
-        Ok(PackedBinary(buffer))
+        if asset_name.contains("windows") {
+            extract_binary_from_zip(&data, asset_name)
+        } else {
+            extract_binary_from_tar_xz(&data)
+        }
     }
 }
 
-/// Extension trait that targets the Typst binary and unpacks it.
-trait Unpack {
-    fn unpack_typst_binary(&mut self) -> StrResult<Vec<u8>>;
+/// Extract the Typst binary from a ZIP archive.
+fn extract_binary_from_zip(data: &[u8], asset_name: &str) -> StrResult<Vec<u8>> {
+    let mut archive = ZipArchive::new(Cursor::new(data))
+        .map_err(|err| eco_format!("failed to extract ZIP archive: {err}"))?;
+
+    let mut file = archive
+        .by_name(&format!("{asset_name}/typst.exe"))
+        .map_err(|_| "ZIP archive did not contain Typst binary")?;
+
+    let mut buffer = vec![];
+    file.read_to_end(&mut buffer).map_err(|err| {
+        eco_format!("failed to read binary data from ZIP archive: {err}")
+    })?;
+
+    Ok(buffer)
 }
 
-impl<R: Read + Seek> Unpack for ZipArchive<R> {
-    fn unpack_typst_binary(&mut self) -> StrResult<Vec<u8>> {
-        let mut binary = self
-            .by_name(&format!("{}/typst.exe", needed_asset()?))
-            .map_err(|_| "asset archive did not contain typst binary")?;
+/// Extract the Typst binary from a `.tar.xz` archive.
+fn extract_binary_from_tar_xz(data: &[u8]) -> StrResult<Vec<u8>> {
+    let mut archive = tar::Archive::new(XzDecoder::new(Cursor::new(data)));
 
-        let mut buffer = Vec::with_capacity(binary.size() as usize);
-        binary
-            .read_to_end(&mut buffer)
-            .map_err(|err| eco_format!("failed to read binary data: {}", err))?;
+    let mut file = archive
+        .entries()
+        .map_err(|err| eco_format!("failed to extract tar.xz archive: {err}"))?
+        .filter_map(Result::ok)
+        .find(|e| e.path().unwrap_or_default().ends_with("typst"))
+        .ok_or("tar.xz archive did not contain Typst binary")?;
 
-        Ok(buffer)
-    }
-}
+    let mut buffer = vec![];
+    file.read_to_end(&mut buffer).map_err(|err| {
+        eco_format!("failed to read binary data from tar.xz archive: {err}")
+    })?;
 
-impl<R: Read> Unpack for tar::Archive<R> {
-    fn unpack_typst_binary(&mut self) -> StrResult<Vec<u8>> {
-        let mut binary = self
-            .entries()
-            .map_err(|err| eco_format!("xz archive corrupted: {}", err))?
-            .filter_map(Result::ok)
-            .find(|e| e.path().unwrap_or_default().ends_with("typst"))
-            .ok_or("asset archive did not contain typst binary")?;
-
-        let mut buffer = Vec::with_capacity(binary.size() as usize);
-        binary
-            .read_to_end(&mut buffer)
-            .map_err(|err| eco_format!("failed to read binary data: {}", err))?;
-
-        Ok(buffer)
-    }
-}
-
-/// The raw binary data from a packed [`Asset`].
-struct PackedBinary(Vec<u8>);
-
-impl PackedBinary {
-    /// Unpacks the asset archive in-memory and writes the uncompressed contents
-    /// into a buffer.
-    ///
-    /// In-memory refers to that the physical archive is never written to disk,
-    /// only the Typst CLI binary will be plucked from the archive and written
-    /// to disk at a later stage, the rest of the archive is discarded.
-    pub fn unpack(self) -> StrResult<Vec<u8>> {
-        let mut raw = BufReader::new(Cursor::new(self.0));
-
-        tar::Archive::new(XzDecoder::new(raw.by_ref()))
-            .unpack_typst_binary()
-            .ok()
-            .or_else(|| {
-                ZipArchive::new(raw)
-                    .ok()
-                    .and_then(|mut archive| archive.unpack_typst_binary().ok())
-            })
-            .ok_or("asset archive unknown or corrupted".into())
-    }
+    Ok(buffer)
 }
 
 /// Determines what asset to download according to the target platform the CLI
@@ -258,6 +222,7 @@ fn update_needed(release: &Release) -> StrResult<bool> {
     Ok(new_tag > current_tag)
 }
 
+/// Path to a possible backup file
 fn backup_path() -> StrResult<PathBuf> {
     #[cfg(target_os = "linux")]
     let root_backup_dir = dirs::state_dir()
