dce7c90ec6ef277b417953d9ba43cedf71e8c0da
Zicklag <zicklag@katharostech.com>
2023-08-25 17:34:29 -0500

    feat: support proxy and custom certificate configuration.
    Respects the `http_proxy` environment variables and adds a CLI
    flag / environment variable that can be used to configure a
    custom trusted certificate chain.

--- crates/typst-cli/src/package.rs
+++ crates/typst-cli/src/package.rs
@@ -1,13 +1,54 @@
 use std::fs;
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use codespan_reporting::term::{self, termcolor};
+use once_cell::sync::Lazy;
 use termcolor::WriteColor;
 use typst::diag::{PackageError, PackageResult};
 use typst::syntax::PackageSpec;
 
 use super::color_stream;
+
+/// HTTP request agent.
+static AGENT: Lazy<ureq::Agent> = Lazy::new(|| {
+    // Get the network proxy config from the environment.
+    let proxy = env_proxy::for_url_str("https://typst.org")
+        .to_url()
+        .and_then(|url| ureq::Proxy::new(url).ok());
+
+    // Check for a custom CA certificate
+    let custom_tls_config = crate::ARGS
+        .cert
+        .as_ref()
+        .map(|path| {
+            let file = std::fs::OpenOptions::new().read(true).open(path)?;
+            let mut buffer = std::io::BufReader::new(file);
+            let certs = rustls_pemfile::certs(&mut buffer)?;
+            let mut cert_store = rustls::RootCertStore::empty();
+            cert_store.add_parsable_certificates(&certs);
+            let config = rustls::ClientConfig::builder()
+                .with_safe_defaults()
+                .with_root_certificates(cert_store)
+                .with_no_client_auth();
+
+            Ok::<_, std::io::Error>(Arc::new(config))
+        })
+        // If there is an error loading certificate, just use the default configuration.
+        .and_then(|x| x.ok());
+
+    // Build the agent
+    let mut agent = ureq::AgentBuilder::new()
+        .user_agent(concat!("typst/{}", env!("CARGO_PKG_VERSION")));
+    if let Some(proxy) = proxy {
+        agent = agent.proxy(proxy);
+    }
+    if let Some(tls_config) = custom_tls_config {
+        agent = agent.tls_config(tls_config);
+    }
+    agent.build()
+});
 
 /// Make a package available in the on-disk cache.
 pub fn prepare_package(spec: &PackageSpec) -> PackageResult<PathBuf> {
@@ -49,7 +90,7 @@ fn download_package(spec: &PackageSpec, package_dir: &Path) -> PackageResult<()>
     );
 
     print_downloading(spec).unwrap();
-    let reader = match ureq::get(&url).call() {
+    let reader = match AGENT.get(&url).call() {
         Ok(response) => response.into_reader(),
         Err(ureq::Error::Status(404, _)) => {
             return Err(PackageError::NotFound(spec.clone()))
