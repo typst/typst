dee50a863b7a6f25e983bb7b9a237e639fa6738d
T0mstone <realt0mstone@gmail.com>
2023-08-26 22:13:30 +0200

    Make version fields actually numbers

--- crates/typst-library/src/lib.rs
+++ crates/typst-library/src/lib.rs
@@ -150,9 +150,18 @@ fn info_module() -> Module {
     let mut scope = Scope::deduplicating();
 
     let mut version_dict = Dict::new();
-    version_dict.insert("major".into(), env!("CARGO_PKG_VERSION_MAJOR").into_value());
-    version_dict.insert("minor".into(), env!("CARGO_PKG_VERSION_MINOR").into_value());
-    version_dict.insert("patch".into(), env!("CARGO_PKG_VERSION_PATCH").into_value());
+    version_dict.insert(
+        "major".into(),
+        env!("CARGO_PKG_VERSION_MAJOR").parse::<u32>().unwrap().into_value(),
+    );
+    version_dict.insert(
+        "minor".into(),
+        env!("CARGO_PKG_VERSION_MINOR").parse::<u32>().unwrap().into_value(),
+    );
+    version_dict.insert(
+        "patch".into(),
+        env!("CARGO_PKG_VERSION_PATCH").parse::<u32>().unwrap().into_value(),
+    );
 
     scope.define("version", version_dict);
     scope.define("version_string", env!("CARGO_PKG_VERSION"));
