280e2146cbbb4f1e465457a263b7fddda0530d45
T0mstone <realt0mstone@gmail.com>
2023-08-31 22:06:25 +0200

    Use git command to obtain commit hash

--- crates/typst-library/build.rs
+++ crates/typst-library/build.rs
@@ -1,26 +1,14 @@
-use gix_odb::loose::Store as Odb;
-use gix_ref::file::{ReferenceExt, Store};
-use gix_ref::store::WriteReflog;
-use std::error::Error;
-use std::fmt::Display;
-use std::path::Path;
-
-fn get_head_commit(root: &Path) -> Result<impl Display, Box<dyn Error>> {
-    let store = Store::at(root.join(".git"), WriteReflog::Disable, Default::default());
-    let odb = Odb::at(root.join(".git").join("objects"), Default::default());
-    let mut head = store.find("HEAD")?;
-    head.peel_to_id_in_place(&store, |oid, buf| {
-        odb.try_find(oid, buf).map(|po| po.map(|data| (data.kind, data.data)))
-    })?;
-    // note: The `Display` impl of `ObjectId` is already hex
-    Ok(head.peeled.unwrap())
-}
+use std::process::Command;
 
 fn main() {
-    // note: the build script starts with its working directory equal to CARGO_MANIFEST_DIR, so crates/typst-library
-    let cwd = std::env::current_dir().unwrap();
-    let root = cwd.parent().unwrap().parent().unwrap();
-    if let Ok(commit) = get_head_commit(root) {
-        println!("cargo:rustc-env=TYPST_COMMIT={commit}");
-    }
+    let hash = Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .and_then(|output| {
+            String::from_utf8(output.stdout.strip_suffix(b"\n").unwrap().into()).ok()
+        })
+        .unwrap_or_else(|| "unknown hash".into());
+    println!("cargo:rustc-env=TYPST_COMMIT={hash}");
 }
