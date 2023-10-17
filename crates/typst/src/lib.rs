9cad5cc83c94837c177d0ede0b1213d1c63bc39a
Myriad-Dreamin <camiyoru@gmail.com>
2023-10-17 21:18:45 +0800

    Add evaluate entry

--- crates/typst/src/lib.rs
+++ crates/typst/src/lib.rs
@@ -59,9 +59,29 @@ use ecow::EcoString;
 
 use crate::diag::{FileResult, SourceResult};
 use crate::doc::Document;
-use crate::eval::{Bytes, Datetime, Library, Route, Tracer};
+use crate::eval::{Bytes, Datetime, Library, Module, Route, Tracer};
 use crate::font::{Font, FontBook};
 use crate::syntax::{FileId, PackageSpec, Source, Span};
+
+/// Evaluate a source file into a module.
+///
+/// - Returns `Ok(module)` if there were no fatal errors.
+/// - Returns `Err(errors)` if there were fatal errors.
+///
+/// Requires a mutable reference to a tracer. Such a tracer can be created with
+/// `Tracer::new()`. Independently of whether compilation succeeded, calling
+/// `tracer.warnings()` after compilation will return all compiler warnings.
+#[tracing::instrument(skip_all)]
+pub fn evaluate(world: &dyn World, tracer: &mut Tracer) -> SourceResult<Module> {
+    let route = Route::default();
+
+    // Call `track` just once to keep comemo's ID stable.
+    let world = world.track();
+    let mut tracer = tracer.track_mut();
+
+    // Evaluate the source file into a module.
+    eval::eval(world, route.track(), TrackedMut::reborrow_mut(&mut tracer), &world.main())
+}
 
 /// Compile a source file into a fully layouted document.
 ///
