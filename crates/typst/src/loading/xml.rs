bbefec6750bdc5b8da228eb58874247d55ac9fb5
Myriad-Dreamin <camiyoru@gmail.com>
2024-01-11 01:32:53 +0800

    Fix break change of xml parse

--- crates/typst/src/loading/xml.rs
+++ crates/typst/src/loading/xml.rs
@@ -1,4 +1,5 @@
 use ecow::EcoString;
+use roxmltree::ParsingOptions;
 
 use crate::diag::{format_xml_like_error, At, FileError, SourceResult};
 use crate::engine::Engine;
@@ -80,8 +81,12 @@ impl xml {
         let text = std::str::from_utf8(data.as_slice())
             .map_err(FileError::from)
             .at(span)?;
-        let document =
-            roxmltree::Document::parse(text).map_err(format_xml_error).at(span)?;
+        let document = roxmltree::Document::parse_with_options(
+            text,
+            ParsingOptions { allow_dtd: true, ..Default::default() },
+        )
+        .map_err(format_xml_error)
+        .at(span)?;
         Ok(convert_xml(document.root()))
     }
 }
