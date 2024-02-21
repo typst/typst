f94320556abd58f84ecce7d24574d22766b95fb4
Jacob Hummer <jcbhmr@outlook.com>
2024-02-21 15:44:54 +0000

    fix review comments

--- crates/typst-docs/src/main.rs
+++ crates/typst-docs/src/main.rs
@@ -8,9 +8,10 @@ use typst_docs::{provide, Html, Resolver};
 use typst_render::render;
 
 struct MyResolver<'a> {
-    out_dir: &'a Path,
+    assets_dir: &'a Path,
     verbose: bool,
 }
+
 impl<'a> Resolver for MyResolver<'a> {
     fn commits(&self, from: &str, to: &str) -> Vec<typst_docs::Commit> {
         if self.verbose {
@@ -18,6 +19,7 @@ impl<'a> Resolver for MyResolver<'a> {
         }
         vec![]
     }
+
     fn example(
         &self,
         hash: u128,
@@ -34,10 +36,10 @@ impl<'a> Resolver for MyResolver<'a> {
         let frame = &document.pages.first().expect("page 0").frame;
         let pixmap = render(frame, 2.0, Color::WHITE);
         let filename = format!("{hash:x}.png");
-        let path = self.out_dir.join("assets").join("docs").join(&filename);
-        create_dir_all(path.parent().expect("parent")).expect("create dir");
+        let path = self.assets_dir.join(&filename);
+        fs::create_dir_all(path.parent().expect("parent")).expect("create dir");
         pixmap.save_png(path.as_path()).expect("save png");
-        let src = format!("/assets/docs/{filename}");
+        let src = format!("/assets/{filename}");
         eprintln!("Generated example image {path:?}");
 
         if let Some(code) = source {
@@ -51,18 +53,20 @@ impl<'a> Resolver for MyResolver<'a> {
             ))
         }
     }
+
     fn image(&self, filename: &str, data: &[u8]) -> String {
         if self.verbose {
             eprintln!("image({filename}, {} bytes)", data.len());
         }
 
-        let path = self.out_dir.join("assets").join("docs").join(filename);
-        create_dir_all(path.parent().expect("parent")).expect("create dir");
-        write(&path, data).expect("write image");
+        let path = self.assets_dir.join(filename);
+        fs::create_dir_all(path.parent().expect("parent")).expect("create dir");
+        fs::write(&path, data).expect("write image");
         eprintln!("Created {} byte image at {path:?}", data.len());
 
-        format!("/assets/docs/{filename}")
+        format!("/assets/{filename}")
     }
+
     fn link(&self, link: &str) -> Option<String> {
         if self.verbose {
             eprintln!("link({link})");
@@ -71,22 +75,23 @@ impl<'a> Resolver for MyResolver<'a> {
     }
 }
 
-/// Generates the JSON representation of the documentation. This can be used
-/// to generate the HTML yourself. You are encouraged to post-process the generated
-/// JSON to rewrite links and other things to match your site's structure.
+/// Generates the JSON representation of the documentation. This can be used to
+/// generate the HTML yourself. You are encouraged to post-process the generated
+/// JSON to rewrite links and other things to match your site's structure. Be
+/// warned: the JSON structure is not stable and may change at any time.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The generation process can produce additional assets. Namely images. This
-    /// option controls where to spit them out. It assumes that this is the base
-    /// folder of the site ("/" in the URL). Images & example renderings will be
-    /// placed in a folder called "assets/docs" under this directory and expected
-    /// to be made available at "/assets/docs/*".
-    #[arg(short, long, default_value = "_site")]
-    out_dir: PathBuf,
+    /// The generation process can produce additional assets. Namely images.
+    /// This option controls where to spit them out. The HTML generation will
+    /// assume that this output directory is served at `/assets/*`. All
+    /// generated HTML references will use `/assets/image5.png` or similar.
+    /// Files will be written to this directory like `${assets_dir}/image5.png`.
+    #[arg(short, long, default_value = "assets")]
+    assets_dir: PathBuf,
 
-    /// Enable verbose logging. This will print out all the calls to the resolver
-    /// and the paths of the generated assets.
+    /// Enable verbose logging. This will print out all the calls to the
+    /// resolver and the paths of the generated assets.
     #[arg(short, long)]
     verbose: bool,
 }
@@ -95,9 +100,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let root_pages = provide(&MyResolver {
-        out_dir: args.out_dir.as_path(),
+        assets_dir: args.assets_dir.as_path(),
         verbose: args.verbose,
     });
+    eprintln!("Be warned: the JSON structure is not stable and may change at any time.");
     let json = serde_json::to_string_pretty(&root_pages)?;
     println!("{json}");
 
