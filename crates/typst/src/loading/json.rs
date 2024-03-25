105d7156f8f9d95e16b3eefdf0fa97e5be7fbe5b
Ana Gelez <ana@gelez.xyz>
2024-03-25 17:51:35 +0100

    Better handle large numbers from external data files (#3791)
    Co-authored-by: Martin Haug <mhaug@live.de>

--- crates/typst/src/loading/json.rs
+++ crates/typst/src/loading/json.rs
@@ -9,13 +9,18 @@ use crate::World;
 
 /// Reads structured data from a JSON file.
 ///
-/// The file must contain a valid JSON object or array. JSON objects will be
-/// converted into Typst dictionaries, and JSON arrays will be converted into
-/// Typst arrays. Strings and booleans will be converted into the Typst
-/// equivalents, `null` will be converted into `{none}`, and numbers will be
-/// converted to floats or integers depending on whether they are whole numbers.
+/// The file must contain a valid JSON value, such as object or array. JSON
+/// objects will be converted into Typst dictionaries, and JSON arrays will be
+/// converted into Typst arrays. Strings and booleans will be converted into the
+/// Typst equivalents, `null` will be converted into `{none}`, and numbers will
+/// be converted to floats or integers depending on whether they are whole
+/// numbers.
 ///
-/// The function returns a dictionary or an array, depending on the JSON file.
+/// Be aware that integers larger than 2<sup>63</sup>-1 will be converted to
+/// floating point numbers, which may result in an approximative value.
+///
+/// The function returns a dictionary, an array or, depending on the JSON file,
+/// another JSON data type.
 ///
 /// The JSON files in the example contain objects with the keys `temperature`,
 /// `unit`, and `weather`.
