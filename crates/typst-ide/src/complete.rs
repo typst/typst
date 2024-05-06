03e3f01d549a21f70f3fec2eecd252bbc3b613c3
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2024-05-06 22:00:51 +0800

    Slice the before_window at char boundaries (#4028)
    Co-authored-by: Laurenz <laurmaedje@gmail.com>

--- crates/typst-ide/src/complete.rs
+++ crates/typst-ide/src/complete.rs
@@ -1053,7 +1053,7 @@ impl<'a> CompletionContext<'a> {
 
     /// A small window of context before the cursor.
     fn before_window(&self, size: usize) -> &str {
-        &self.before[self.cursor.saturating_sub(size)..]
+        Scanner::new(self.before).from(self.cursor.saturating_sub(size))
     }
 
     /// Add a prefix and suffix to all applications.
@@ -1432,5 +1432,13 @@ mod tests {
     fn test_autocomplete() {
         test("#i", 2, &["int", "if conditional"], &["foo"]);
         test("#().", 4, &["insert", "remove", "len", "all"], &["foo"]);
+    }
+
+    #[test]
+    fn test_before_window_char_boundary() {
+        // Check that the `before_window` doesn't slice into invalid byte
+        // boundaries.
+        let s = "😀😀     #text(font: \"\")";
+        test(s, s.len() - 2, &[], &[]);
     }
 }
