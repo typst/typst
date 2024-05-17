48e0d22a2a5e53b02d31befbe6bed187c6f3ff77
Ilia <istudyatuni@gmail.com>
2024-05-17 08:12:08 +0300

    Add ability to choose between minified and pretty-printed JSON

--- crates/typst-cli/src/args.rs
+++ crates/typst-cli/src/args.rs
@@ -153,6 +153,10 @@ pub struct QueryCommand {
     /// The format to serialize in
     #[clap(long = "format", default_value = "json")]
     pub format: SerializationFormat,
+
+    /// Pretty-print JSON
+    #[clap(long)]
+    pub pretty: bool,
 }
 
 // Output file format for query command
