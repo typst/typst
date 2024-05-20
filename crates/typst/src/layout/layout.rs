d13b6495e56532310b77866075fc288947bd5c17
Malo <57839069+MDLC01@users.noreply.github.com>
2024-05-20 16:02:35 +0200

    Add missing "is"
    Co-authored-by: Eric Biedert <github@ericbiedert.de>

--- crates/typst/src/layout/layout.rs
+++ crates/typst/src/layout/layout.rs
@@ -29,7 +29,7 @@ use crate::syntax::Span;
 ///
 /// If the `layout` call is placed inside a box with a width of `{800pt}` and a
 /// height of `{400pt}`, then the specified function will be given the parameter
-/// `{(width: 800pt, height: 400pt)}`. If it placed directly into the page, it
+/// `{(width: 800pt, height: 400pt)}`. If it is placed directly into the page, it
 /// receives the page's dimensions minus its margins. This is mostly useful in
 /// combination with [measurement]($measure).
 ///
