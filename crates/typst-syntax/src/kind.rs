9c7dfadb6db23978c206e1d9927eaf41d8149e8c
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-05-21 23:54:20 -0300

    reorder syntax kind

--- crates/typst-syntax/src/kind.rs
+++ crates/typst-syntax/src/kind.rs
@@ -4,6 +4,16 @@
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 #[repr(u8)]
 pub enum SyntaxKind {
+    /// The end of token stream.
+    End,
+    /// An invalid sequence of characters.
+    Error,
+
+    /// A line comment: `// ...`.
+    LineComment,
+    /// A block comment: `/* ... */`.
+    BlockComment,
+
     /// The contents of a file or content block.
     Markup,
     /// Plain text without markup.
@@ -252,6 +262,10 @@ pub enum SyntaxKind {
     ModuleImport,
     /// Items to import from a module: `a, b, c`.
     ImportItems,
+    /// A path to an imported name from a submodule: `a.b.c`.
+    ImportItemPath,
+    /// A renamed import item: `a as d`.
+    RenamedImportItem,
     /// A module include: `include "chapter1.typ"`.
     ModuleInclude,
     /// A break from a loop: `break`.
@@ -264,20 +278,6 @@ pub enum SyntaxKind {
     Destructuring,
     /// A destructuring assignment expression: `(x, y) = (1, 2)`.
     DestructAssignment,
-
-    /// A line comment: `// ...`.
-    LineComment,
-    /// A block comment: `/* ... */`.
-    BlockComment,
-    /// An invalid sequence of characters.
-    Error,
-    /// The end of token stream.
-    End,
-
-    /// A path to an imported name from a submodule: `a.b.c`.
-    ImportItemPath,
-    /// A renamed import item: `a as d`.
-    RenamedImportItem,
 }
 
 impl SyntaxKind {
@@ -355,7 +355,7 @@ impl SyntaxKind {
     pub fn is_trivia(self) -> bool {
         matches!(
             self,
-            Self::Space | Self::Parbreak | Self::LineComment | Self::BlockComment
+            Self::LineComment | Self::BlockComment | Self::Space | Self::Parbreak
         )
     }
 
@@ -367,6 +367,10 @@ impl SyntaxKind {
     /// A human-readable name for the kind.
     pub fn name(self) -> &'static str {
         match self {
+            Self::End => "end of tokens",
+            Self::Error => "syntax error",
+            Self::LineComment => "line comment",
+            Self::BlockComment => "block comment",
             Self::Markup => "markup",
             Self::Text => "text",
             Self::Space => "space",
@@ -494,10 +498,6 @@ impl SyntaxKind {
             Self::FuncReturn => "`return` expression",
             Self::Destructuring => "destructuring pattern",
             Self::DestructAssignment => "destructuring assignment expression",
-            Self::LineComment => "line comment",
-            Self::BlockComment => "block comment",
-            Self::Error => "syntax error",
-            Self::End => "end of tokens",
         }
     }
 }
