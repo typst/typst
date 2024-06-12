d501f44960093a5c5be49270a504ba0717946501
tinger <me@tinger.dev>
2024-06-12 11:38:19 +0200

    Add ExportTarget

--- crates/typst/src/foundations/sys.rs
+++ crates/typst/src/foundations/sys.rs
@@ -1,9 +1,10 @@
 //! System-related things.
 
 use crate::foundations::{Dict, Module, Scope, Version};
+use crate::ExportTarget;
 
 /// A module with system-related things.
-pub fn module(inputs: Dict) -> Module {
+pub fn module(inputs: Dict, target: ExportTarget) -> Module {
     let mut scope = Scope::deduplicating();
     scope.define(
         "version",
@@ -14,5 +15,6 @@ pub fn module(inputs: Dict) -> Module {
         ]),
     );
     scope.define("inputs", inputs);
+    scope.define("target", target.as_str());
     Module::new("sys", scope)
 }
