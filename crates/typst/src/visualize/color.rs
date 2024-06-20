9ed7f1713b96ca0957b94bddc3a699d6e0b2bc10
Eric Biedert <github@ericbiedert.de>
2024-06-20 20:02:22 +0200

    Fix color mixing and gradients in luma space

--- crates/typst/src/visualize/color.rs
+++ crates/typst/src/visualize/color.rs
@@ -1191,7 +1191,7 @@ impl Color {
                 Color::Hsv(Hsv::new(RgbHue::from_degrees(m[0]), m[1], m[2], m[3]))
             }
             ColorSpace::Cmyk => Color::Cmyk(Cmyk::new(m[0], m[1], m[2], m[3])),
-            ColorSpace::D65Gray => Color::Luma(Luma::new(m[0], m[1])),
+            ColorSpace::D65Gray => Color::Luma(Luma::new(m[0], m[3])),
         })
     }
 
