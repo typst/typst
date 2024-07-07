1f5008881f76bc75d9042e9eefca4bece77ea0dc
Andrew Voynov <andrewvoynov.b@gmail.com>
2024-07-07 09:51:25 +0300

    feat(entrypoint): added `raw` arg to return `bytes`

--- crates/typst/src/loading/entrypoint.rs
+++ crates/typst/src/loading/entrypoint.rs
@@ -2,12 +2,22 @@
 
 // use ecow::EcoString;
 
-// use crate::diag::{At, SourceResult};
+use crate::diag::SourceResult;
 use crate::engine::Engine;
-use crate::foundations::func;
+use crate::foundations::{func, Bytes, Str};
+use typst_macros::cast;
 // use crate::loading::{Encoding, Readable};
 // use crate::syntax::Spanned;
 use crate::World;
+
+/// A value that can be read from a file.
+#[derive(Debug, Clone, PartialEq, Hash)]
+pub enum Entrypoint {
+    /// Lossy string.
+    Str(Str),
+    /// Raw bytes.
+    Bytes(Bytes),
+}
 
 /// Returns absolute path to the  main file that represents document's entry
 /// point. Returns `{none}` if `stdin` is used instead.
@@ -15,26 +25,30 @@ use crate::World;
 pub fn entrypoint(
     /// The engine.
     engine: &mut Engine,
-    // /// The encoding to read the file with.
-    // ///
-    // /// If set to `{none}`, this function returns raw bytes.
-    // #[named]
-    // #[default(Some(Encoding::Utf8))]
-    // encoding: Option<Encoding>,
-    // ) -> SourceResult<Readable> {
-) -> Option<String> {
+    /// Whether to return `{bytes}` or not.
+    ///
+    /// If set to `{true}`, this function returns raw bytes. Default is
+    /// `{false}`. Useful when path cannot be a valid UTF-8 string.
+    #[named]
+    #[default(false)]
+    raw: bool,
+) -> SourceResult<Option<Entrypoint>> {
     // let Spanned { v: path, span } = path;
     let file_id = engine.world.main().id();
     if engine.world.main().is_stdin() {
-        return None;
+        return Ok(None);
     }
     dbg!(&file_id);
     let rootless_path = file_id.vpath().as_rootless_path();
     dbg!(rootless_path);
     let rooted_path = file_id.vpath().as_rooted_path();
     dbg!(rooted_path);
+    if raw {
+        let bytes: Vec<u8> = rooted_path.as_os_str().as_encoded_bytes().to_vec();
+        return Ok(Some(Entrypoint::Bytes(bytes.into())));
+    }
     let string = rooted_path.to_string_lossy();
-    Some(string.into())
+    Ok(Some(Entrypoint::Str(string.into())))
     // let data = engine.world.file(id).at(span)?;
     // Ok(match encoding {
     //     None => Readable::Bytes(data),
@@ -45,6 +59,16 @@ pub fn entrypoint(
     //             .into(),
     //     ),
     // })
+}
+
+cast! {
+    Entrypoint,
+    self => match self {
+        Self::Str(v) => v.into_value(),
+        Self::Bytes(v) => v.into_value(),
+    },
+    v: Str => Self::Str(v),
+    v: Bytes => Self::Bytes(v),
 }
 
 // /// An encoding of a file.
