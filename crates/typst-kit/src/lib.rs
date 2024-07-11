47c62dba133738b88f4cf0bd4c046dc7ecf40e86
tinger <me@tinger.dev>
2024-07-11 14:34:47 +0200

    Add typst-kit crate

---
+++ crates/typst-kit/src/lib.rs
@@ -0,0 +1,4 @@
+//! Typst-kit contains various default implementations of functionality used in
+//! typst-cli. It is intended as a single source of truth for things like font
+//! searching, package downloads and more. Each component of typst-kit is
+//! optional but enabled by default.
