81a568dabb72589038a8f259cbf76cce54a58d55
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2024-07-16 23:55:23 +0100

    solve another obvious error

--- crates/typst-pdf/src/gradient.rs
+++ crates/typst-pdf/src/gradient.rs
@@ -394,16 +394,16 @@ fn write_patch(
     // TODO: check if it is correct
     // TODO: find a better solution
     for x in c0 {
-        target.extend_from_slice(&[(x >> 255) as u8, (x & 255) as u8]);
+        target.extend_from_slice(&[(x >> 8) as u8, (x & 255) as u8]);
     }
     for x in c0 {
-        target.extend_from_slice(&[(x >> 255) as u8, (x & 255) as u8]);
+        target.extend_from_slice(&[(x >> 8) as u8, (x & 255) as u8]);
     }
     for x in c1 {
-        target.extend_from_slice(&[(x >> 255) as u8, (x & 255) as u8]);
+        target.extend_from_slice(&[(x >> 8) as u8, (x & 255) as u8]);
     }
     for x in c1 {
-        target.extend_from_slice(&[(x >> 255) as u8, (x & 255) as u8]);
+        target.extend_from_slice(&[(x >> 8) as u8, (x & 255) as u8]);
     }
 }
 
