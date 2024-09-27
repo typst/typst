typst/typst

commit 53135a0813c709a6e2762d806e991a6441dc27de

Implement symbol Proposal 21 (iteration 1) (#5053)

crates/typst/src/symbols/sym.rs
@@ -341,18 +341,18 @@ pub(crate) const SYM: &[(&str, Symbol)] = symbols! {
     // Set theory.
     emptyset: [
         '∅',
+        arrow.r: '⦳',
+        arror.l: '⦴',
         bar: '⦱',
         circle: '⦲',
-        larrow: '⦴',
-        rarrow: '⦳',
         rev: '⦰',
     ],
     nothing: [
         '∅',
+        arrow.r: '⦳',
+        arror.l: '⦴',
         bar: '⦱',
         circle: '⦲',
-        larrow: '⦴',
-        rarrow: '⦳',
         rev: '⦰',
     ],
     without: '∖',

commit f5956577a19f4d5e088a72590e26964da576ad12

Fix a typo introduced in #5053

crates/typst/src/symbols/sym.rs
@@ -342,7 +342,7 @@ pub(crate) const SYM: &[(&str, Symbol)] = symbols! {
     emptyset: [
         '∅',
         arrow.r: '⦳',
-        arror.l: '⦴',
+        arrow.l: '⦴',
         bar: '⦱',
         circle: '⦲',
         rev: '⦰',
@@ -350,7 +350,7 @@ pub(crate) const SYM: &[(&str, Symbol)] = symbols! {
     nothing: [
         '∅',
         arrow.r: '⦳',
-        arror.l: '⦴',
+        arrow.l: '⦴',
         bar: '⦱',
         circle: '⦲',
         rev: '⦰',