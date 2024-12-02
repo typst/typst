9cf6eaff23ac75890ba1a7880f5ef93386897d41
Ian Wrzesinski <wrzian@umich.edu>
2024-12-02 13:31:18 -0500

    updates to CapturesVisitor to make capturing undefined variables more explicit

--- crates/typst-ide/src/tooltip.rs
+++ crates/typst-ide/src/tooltip.rs
@@ -3,7 +3,7 @@ use std::fmt::Write;
 use ecow::{eco_format, EcoString};
 use if_chain::if_chain;
 use typst::engine::Sink;
-use typst::foundations::{repr, Capturer, CastInfo, Repr, Value};
+use typst::foundations::{repr, Capturer, CastInfo, Repr, Scopes, Value};
 use typst::layout::Length;
 use typst::model::Document;
 use typst::syntax::ast::AstNode;
@@ -143,17 +143,22 @@ fn closure_tooltip(leaf: &LinkedNode) -> Option<Tooltip> {
     }
 
     // Analyze the closure's captures.
-    let mut visitor = CapturesVisitor::new(None, Capturer::Function);
+    let empty = Scopes::new(None);
+    let mut undef = Vec::new();
+    // Since we passs an empty scope, all variables will be added to `undef`.
+    let mut visitor = CapturesVisitor::new(&empty, Capturer::Function, Some(&mut undef));
     visitor.visit(parent);
+    let captues = visitor.finish();
+    assert!(captues.iter().next().is_none());
 
-    let captures = visitor.finish();
     let mut names: Vec<_> =
-        captures.iter().map(|(name, ..)| eco_format!("`{name}`")).collect();
+        undef.iter().map(|(name, _span)| eco_format!("`{name}`")).collect();
     if names.is_empty() {
         return None;
     }
 
     names.sort();
+    names.dedup();
 
     let tooltip = repr::separated_list(&names, "and");
     Some(Tooltip::Text(eco_format!("This closure captures {tooltip}")))
