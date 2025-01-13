33d035c764cea9bc7e8d163c823dd31ae40196f6
Laurenz <laurmaedje@gmail.com>
2025-01-13 20:14:02 +0100

    Reorganize file

--- crates/typst-timing/src/lib.rs
+++ crates/typst-timing/src/lib.rs
@@ -8,6 +8,41 @@ use parking_lot::Mutex;
 use serde::ser::SerializeSeq;
 use serde::{Serialize, Serializer};
 
+/// Creates a timing scope around an expression.
+///
+/// The output of the expression is returned.
+///
+/// The scope will be named `name` and will have the span `span`. The span is
+/// optional.
+///
+/// ## Example
+///
+/// ```rs
+/// // With a scope name and span.
+/// timed!(
+///     "my scope",
+///     span = Span::detached(),
+///     std::thread::sleep(std::time::Duration::from_secs(1)),
+/// );
+///
+/// // With a scope name and no span.
+/// timed!(
+///     "my scope",
+///     std::thread::sleep(std::time::Duration::from_secs(1)),
+/// );
+/// ```
+#[macro_export]
+macro_rules! timed {
+    ($name:expr, span = $span:expr, $body:expr $(,)?) => {{
+        let __scope = $crate::TimingScope::with_span($name, Some($span));
+        $body
+    }};
+    ($name:expr, $body:expr $(,)?) => {{
+        let __scope = $crate::TimingScope::new($name);
+        $body
+    }};
+}
+
 thread_local! {
     /// Data that is initialized once per thread.
     static THREAD_DATA: ThreadData = ThreadData {
@@ -28,40 +63,6 @@ static ENABLED: AtomicBool = AtomicBool::new(false);
 /// The list of collected events.
 static EVENTS: Mutex<Vec<Event>> = Mutex::new(Vec::new());
 
-/// Per-thread data.
-struct ThreadData {
-    /// The thread's ID.
-    ///
-    /// In contrast to `std::thread::current().id()`, this is wasm-compatible
-    /// and also a bit cheaper to access because the std version does a bit more
-    /// stuff (including cloning an `Arc`).
-    id: u64,
-    /// A way to get the time from WebAssembly.
-    #[cfg(all(target_arch = "wasm32", feature = "wasm"))]
-    timer: WasmTimer,
-}
-
-/// An event that has been recorded.
-struct Event {
-    /// Whether this is a start or end event.
-    kind: EventKind,
-    /// The time at which this event occurred.
-    timestamp: Timestamp,
-    /// The name of this event.
-    name: &'static str,
-    /// The raw value of the span of code that this event was recorded in.
-    span: Option<NonZeroU64>,
-    /// The thread ID of this event.
-    thread_id: u64,
-}
-
-/// Whether an event marks the start or end of a scope.
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-enum EventKind {
-    Start,
-    End,
-}
-
 /// Enable the timer.
 #[inline]
 pub fn enable() {
@@ -80,6 +81,61 @@ pub fn is_enabled() -> bool {
 #[inline]
 pub fn clear() {
     EVENTS.lock().clear();
+}
+
+/// Export data as JSON for Chrome's tracing tool.
+///
+/// The `source` function is called for each span to get the source code
+/// location of the span. The first element of the tuple is the file path and
+/// the second element is the line number.
+pub fn export_json<W: Write>(
+    writer: W,
+    mut source: impl FnMut(NonZeroU64) -> (String, u32),
+) -> Result<(), String> {
+    #[derive(Serialize)]
+    struct Entry {
+        name: &'static str,
+        cat: &'static str,
+        ph: &'static str,
+        ts: f64,
+        pid: u64,
+        tid: u64,
+        args: Option<Args>,
+    }
+
+    #[derive(Serialize)]
+    struct Args {
+        file: String,
+        line: u32,
+    }
+
+    let lock = EVENTS.lock();
+    let events = lock.as_slice();
+
+    let mut serializer = serde_json::Serializer::new(writer);
+    let mut seq = serializer
+        .serialize_seq(Some(events.len()))
+        .map_err(|e| format!("failed to serialize events: {e}"))?;
+
+    for event in events.iter() {
+        seq.serialize_element(&Entry {
+            name: event.name,
+            cat: "typst",
+            ph: match event.kind {
+                EventKind::Start => "B",
+                EventKind::End => "E",
+            },
+            ts: event.timestamp.micros_since(events[0].timestamp),
+            pid: 1,
+            tid: event.thread_id,
+            args: event.span.map(&mut source).map(|(file, line)| Args { file, line }),
+        })
+        .map_err(|e| format!("failed to serialize event: {e}"))?;
+    }
+
+    seq.end().map_err(|e| format!("failed to serialize events: {e}"))?;
+
+    Ok(())
 }
 
 /// A scope that records an event when it is dropped.
@@ -137,6 +193,27 @@ impl Drop for TimingScope {
     }
 }
 
+/// An event that has been recorded.
+struct Event {
+    /// Whether this is a start or end event.
+    kind: EventKind,
+    /// The time at which this event occurred.
+    timestamp: Timestamp,
+    /// The name of this event.
+    name: &'static str,
+    /// The raw value of the span of code that this event was recorded in.
+    span: Option<NonZeroU64>,
+    /// The thread ID of this event.
+    thread_id: u64,
+}
+
+/// Whether an event marks the start or end of a scope.
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum EventKind {
+    Start,
+    End,
+}
+
 /// A cross-platform way to get the current time.
 #[derive(Copy, Clone)]
 struct Timestamp {
@@ -181,94 +258,17 @@ impl Timestamp {
     }
 }
 
-/// Creates a timing scope around an expression.
-///
-/// The output of the expression is returned.
-///
-/// The scope will be named `name` and will have the span `span`. The span is
-/// optional.
-///
-/// ## Example
-///
-/// ```rs
-/// // With a scope name and span.
-/// timed!(
-///     "my scope",
-///     span = Span::detached(),
-///     std::thread::sleep(std::time::Duration::from_secs(1)),
-/// );
-///
-/// // With a scope name and no span.
-/// timed!(
-///     "my scope",
-///     std::thread::sleep(std::time::Duration::from_secs(1)),
-/// );
-/// ```
-#[macro_export]
-macro_rules! timed {
-    ($name:expr, span = $span:expr, $body:expr $(,)?) => {{
-        let __scope = $crate::TimingScope::with_span($name, Some($span));
-        $body
-    }};
-    ($name:expr, $body:expr $(,)?) => {{
-        let __scope = $crate::TimingScope::new($name);
-        $body
-    }};
-}
-
-/// Export data as JSON for Chrome's tracing tool.
-///
-/// The `source` function is called for each span to get the source code
-/// location of the span. The first element of the tuple is the file path and
-/// the second element is the line number.
-pub fn export_json<W: Write>(
-    writer: W,
-    mut source: impl FnMut(NonZeroU64) -> (String, u32),
-) -> Result<(), String> {
-    #[derive(Serialize)]
-    struct Entry {
-        name: &'static str,
-        cat: &'static str,
-        ph: &'static str,
-        ts: f64,
-        pid: u64,
-        tid: u64,
-        args: Option<Args>,
-    }
-
-    #[derive(Serialize)]
-    struct Args {
-        file: String,
-        line: u32,
-    }
-
-    let lock = EVENTS.lock();
-    let events = lock.as_slice();
-
-    let mut serializer = serde_json::Serializer::new(writer);
-    let mut seq = serializer
-        .serialize_seq(Some(events.len()))
-        .map_err(|e| format!("failed to serialize events: {e}"))?;
-
-    for event in events.iter() {
-        seq.serialize_element(&Entry {
-            name: event.name,
-            cat: "typst",
-            ph: match event.kind {
-                EventKind::Start => "B",
-                EventKind::End => "E",
-            },
-            ts: event.timestamp.micros_since(events[0].timestamp),
-            pid: 1,
-            tid: event.thread_id,
-            args: event.span.map(&mut source).map(|(file, line)| Args { file, line }),
-        })
-        .map_err(|e| format!("failed to serialize event: {e}"))?;
-    }
-
-    seq.end().map_err(|e| format!("failed to serialize events: {e}"))?;
-
-    Ok(())
+/// Per-thread data.
+struct ThreadData {
+    /// The thread's ID.
+    ///
+    /// In contrast to `std::thread::current().id()`, this is wasm-compatible
+    /// and also a bit cheaper to access because the std version does a bit more
+    /// stuff (including cloning an `Arc`).
+    id: u64,
+    /// A way to get the time from WebAssembly.
+    #[cfg(all(target_arch = "wasm32", feature = "wasm"))]
+    timer: WasmTimer,
 }
 
 /// A way to get the time from WebAssembly.
