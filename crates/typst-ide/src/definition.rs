9cf58c7967b5428f0619487174e8acd65c0468d7
Myriad-Dreamin <camiyoru@gmail.com>
2025-03-31 00:54:29 +0800

    Update tests

--- crates/typst-ide/src/definition.rs
+++ crates/typst-ide/src/definition.rs
@@ -184,13 +184,13 @@ mod tests {
 
     #[test]
     fn test_definition_ref() {
-        test("#figure[] <hi> See @hi", -2, Side::After).must_be_at("main.typ", 1..9);
+        test("#figure[] <hi> See @hi", -2, Side::After).must_be_at("main.typ", 10..14);
         let source =
             r#"#let test1(body) = figure(body); #test1([Test1]) <fig:test1> @fig:test1"#;
-        test(source, -2, Side::After).must_be_at("main.typ", 19..31);
+        test(source, -2, Side::After).must_be_at("main.typ", 49..60);
         let source = r#"#let test1(body) = figure(body); #test1([Test1]) <fig:test1> @fig:test1
 #let test2(body) = test1(body); #test2([Test2]) <fig:test2>; @fig:test2"#;
-        test(source, -2, Side::After).must_be_at("main.typ", 19..31);
+        test(source, -2, Side::After).must_be_at("main.typ", 120..131);
     }
 
     #[test]
