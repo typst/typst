516aadd7eaafee7c8b5829b48cac8bfbc5315d1d
cAttte <26514199+cAttte@users.noreply.github.com>
2025-06-08 18:01:22 -0300

    test(typst-ide): add test for new math completion logic

--- crates/typst-ide/src/complete.rs
+++ crates/typst-ide/src/complete.rs
@@ -1673,6 +1673,13 @@ mod tests {
         test("#{() .a}", -2).must_include(["at", "any", "all"]);
     }
 
+    /// Test that autocomplete in math uses the correct global scope.
+    #[test]
+    fn test_autocomplete_math_scope() {
+        test("$ #col $", -3).must_include(["colbreak"]).must_exclude(["colon"]);
+        test("$ col $", -3).must_include(["colon"]).must_exclude(["colbreak"]);
+    }
+
     /// Test that the `before_window` doesn't slice into invalid byte
     /// boundaries.
     #[test]
