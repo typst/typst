42b45473c7cb0c7c942c01e1b01029c7c293f9fd
Laurenz <laurmaedje@gmail.com>
2025-07-18 15:16:52 +0200

    Revert "Prepare twice"
    This reverts commit e6b72525022772e7611d98e1ebf7358d5471ea21.

--- crates/typst-realize/src/lib.rs
+++ crates/typst-realize/src/lib.rs
@@ -356,14 +356,6 @@ fn visit_show_rules<'a>(
     // prepare it.
     let mut tags = None;
     if !prepared {
-        std::hint::black_box(prepare(
-            s.engine,
-            s.locator,
-            output.clone().to_mut(),
-            &mut map.clone(),
-            styles,
-        ));
-
         tags = prepare(s.engine, s.locator, output.to_mut(), &mut map, styles)?;
     }
 
