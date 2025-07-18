ed031e4fcde36e213c43eb170cd4d3ab00a02a63
Laurenz <laurmaedje@gmail.com>
2025-07-18 15:16:55 +0200

    Revert "Introspect twice"
    This reverts commit 47f7d085f40f5b55f7ab042b469ad19c05e539f3.

--- crates/typst-layout/src/pages/mod.rs
+++ crates/typst-layout/src/pages/mod.rs
@@ -86,7 +86,6 @@ fn layout_document_impl(
     )?;
 
     let pages = layout_pages(&mut engine, &mut children, &mut locator, styles)?;
-    std::hint::black_box(introspect_pages(&pages));
     let introspector = introspect_pages(&pages);
 
     Ok(PagedDocument { pages, info, introspector })
