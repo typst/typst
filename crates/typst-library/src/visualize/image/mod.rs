65f56b6f8b1f3f90b98e51fb287aba8f28c07080
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-07-22 11:56:47 +0200

    Remove unused import

--- crates/typst-library/src/visualize/image/mod.rs
+++ crates/typst-library/src/visualize/image/mod.rs
@@ -16,7 +16,7 @@ use std::sync::Arc;
 
 use ecow::EcoString;
 use hayro_syntax::LoadPdfError;
-use typst_library::{Feature, World};
+use typst_library::World;
 use typst_syntax::{Span, Spanned};
 use typst_utils::LazyHash;
 
