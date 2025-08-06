298c293181a24c6ba7325820dccefb3611bfeda3
Laurenz <laurmaedje@gmail.com>
2025-08-06 14:32:39 +0200

    Support smartquotes in HTML export (#6710)
    Co-authored-by: Malo <57839069+MDLC01@users.noreply.github.com>

--- crates/typst-html/src/convert.rs
+++ crates/typst-html/src/convert.rs
@@ -5,20 +5,24 @@ use typst_library::foundations::{Content, StyleChain, Target, TargetElem};
 use typst_library::introspection::{SplitLocator, TagElem};
 use typst_library::layout::{Abs, Axes, Region, Size};
 use typst_library::routines::Pair;
-use typst_library::text::{LinebreakElem, SmartQuoteElem, SpaceElem, TextElem};
+use typst_library::text::{
+    LinebreakElem, SmartQuoteElem, SmartQuoter, SmartQuotes, SpaceElem, TextElem,
+    is_default_ignorable,
+};
 
-use crate::fragment::html_fragment;
+use crate::fragment::{html_block_fragment, html_inline_fragment};
 use crate::{FrameElem, HtmlElem, HtmlElement, HtmlFrame, HtmlNode, tag};
 
 /// Converts realized content into HTML nodes.
 pub fn convert_to_nodes<'a>(
     engine: &mut Engine,
     locator: &mut SplitLocator,
+    quoter: &mut SmartQuoter,
     children: impl IntoIterator<Item = Pair<'a>>,
 ) -> SourceResult<EcoVec<HtmlNode>> {
     let mut output = EcoVec::new();
     for (child, styles) in children {
-        handle(engine, child, locator, styles, &mut output)?;
+        handle(engine, child, locator, styles, quoter, &mut output)?;
     }
     Ok(output)
 }
@@ -29,6 +33,7 @@ fn handle(
     child: &Content,
     locator: &mut SplitLocator,
     styles: StyleChain,
+    quoter: &mut SmartQuoter,
     output: &mut EcoVec<HtmlNode>,
 ) -> SourceResult<()> {
     if let Some(elem) = child.to_packed::<TagElem>() {
@@ -36,7 +41,22 @@ fn handle(
     } else if let Some(elem) = child.to_packed::<HtmlElem>() {
         let mut children = EcoVec::new();
         if let Some(body) = elem.body.get_ref(styles) {
-            children = html_fragment(engine, body, locator.next(&elem.span()), styles)?;
+            if tag::is_block_by_default(elem.tag) {
+                children = html_block_fragment(
+                    engine,
+                    body,
+                    locator.next(&elem.span()),
+                    styles,
+                )?;
+
+                // Block-level elements reset the smart quoting state. This part
+                // is unfortunately untested as it's currently not possible to
+                // create inline-level content next to block-level content
+                // without a paragraph automatically appearing.
+                *quoter = SmartQuoter::new();
+            } else {
+                children = html_inline_fragment(engine, body, locator, quoter, styles)?;
+            }
         }
         let element = HtmlElement {
             tag: elem.tag,
@@ -57,10 +77,20 @@ fn handle(
     } else if let Some(elem) = child.to_packed::<LinebreakElem>() {
         output.push(HtmlElement::new(tag::br).spanned(elem.span()).into());
     } else if let Some(elem) = child.to_packed::<SmartQuoteElem>() {
-        output.push(HtmlNode::text(
-            if elem.double.get(styles) { '"' } else { '\'' },
-            child.span(),
-        ));
+        let double = elem.double.get(styles);
+        if elem.enabled.get(styles) {
+            let before = last_char(output);
+            let quotes = SmartQuotes::get(
+                elem.quotes.get_ref(styles),
+                styles.get(TextElem::lang),
+                styles.get(TextElem::region),
+                elem.alternative.get(styles),
+            );
+            let quote = quoter.quote(before, &quotes, double);
+            output.push(HtmlNode::text(quote, child.span()));
+        } else {
+            output.push(HtmlNode::text(if double { '"' } else { '\'' }, child.span()));
+        }
     } else if let Some(elem) = child.to_packed::<FrameElem>() {
         let locator = locator.next(&elem.span());
         let style = TargetElem::target.set(Target::Paged).wrap();
@@ -80,6 +110,20 @@ fn handle(
         ));
     }
     Ok(())
+}
+
+/// Returns the last non-default ignorable character from the passed nodes.
+fn last_char(nodes: &[HtmlNode]) -> Option<char> {
+    for node in nodes.iter().rev() {
+        if let Some(c) = match node {
+            HtmlNode::Text(s, _) => s.chars().rev().find(|&c| !is_default_ignorable(c)),
+            HtmlNode::Element(e) => last_char(&e.children),
+            _ => None,
+        } {
+            return Some(c);
+        }
+    }
+    None
 }
 
 /// Checks whether the given element is an inline-level HTML element.
