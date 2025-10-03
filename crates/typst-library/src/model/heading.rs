aa509691f5c01b457dbe24e5047e3086f6f8723c
Laurenz <laurmaedje@gmail.com>
2025-10-03 11:31:20 +0200

    Rephrase a bit

--- crates/typst-library/src/model/heading.rs
+++ crates/typst-library/src/model/heading.rs
@@ -29,11 +29,11 @@ use crate::text::{FontWeight, LocalName, TextElem, TextSize};
 /// [outline] of all headings for you. To exclude one or more headings from this
 /// outline, you can set the `outlined` parameter to `{false}`.
 ///
-/// When using a [show rule]($styling/#show-rules) to customize the content
-/// of a heading, make sure to wrap the content in a [`block`]($block) (which
-/// is implicitly [sticky]($block.sticky) for headings). This avoids that the
-/// heading becomes an "orphan", that is, it remains at the end of the page
-/// but subsequent content is wrapped to the next page.
+/// When using a [show rule]($styling/#show-rules) to customize the content of a
+/// heading, make sure to wrap the content in a [`block`]($block) (which is
+/// implicitly [sticky]($block.sticky) for headings through a built-in show-set
+/// rule). This prevents the heading from becoming an "orphan", i.e. remaining
+/// at the end of the page with the following content being on the next page.
 ///
 /// # Example
 /// ```example
