typst/typst

commit 838a46dbb7124125947bfdafe8ddf97810c5de47

Test all exif rotation types and fix two of them (#6102)

crates/typst-library/src/visualize/image/raster.rs
@@ -325,12 +325,12 @@ fn apply_rotation(image: &mut DynamicImage, rotation: u32) {
             ops::flip_horizontal_in_place(image);
             *image = image.rotate270();
         }
-        6 => *image = image.rotate90(),
+        6 => *image = image.rotate270(),
         7 => {
             ops::flip_horizontal_in_place(image);
             *image = image.rotate90();
         }
-        8 => *image = image.rotate270(),
+        8 => *image = image.rotate90(),
         _ => {}
     }
 }

Cargo.toml
@@ -33,7 +33,7 @@ typst-syntax = { path = "crates/typst-syntax", version = "0.13.1" }
 typst-timing = { path = "crates/typst-timing", version = "0.13.1" }
 typst-utils = { path = "crates/typst-utils", version = "0.13.1" }
 typst-assets = { git = "https://github.com/typst/typst-assets", rev = "ab1295f" }
-typst-dev-assets = { git = "https://github.com/typst/typst-dev-assets", rev = "9879589" }
+typst-dev-assets = { git = "https://github.com/typst/typst-dev-assets", rev = "fddbf8b" }
 arrayvec = "0.7.4"
 az = "1.2"
 base64 = "0.22"

tests/suite/visualize/image.typ
@@ -247,12 +247,6 @@ A #box(image("/assets/images/tiger.jpg", height: 1cm, width: 80%)) B
   format: "rgba8",
 )
 
---- issue-870-image-rotation ---
-// Ensure that EXIF rotation is applied.
-// https://github.com/image-rs/image/issues/1045
-// File is from https://magnushoff.com/articles/jpeg-orientation/
-#image("/assets/images/f2t.jpg", width: 10pt)
-
 --- issue-measure-image ---
 // Test that image measurement doesn't turn `inf / some-value` into 0pt.
 #context {
@@ -267,3 +261,16 @@ A #box(image("/assets/images/tiger.jpg", height: 1cm, width: 80%)) B
 --- issue-3733-dpi-svg ---
 #set page(width: 200pt, height: 200pt, margin: 0pt)
 #image("/assets/images/relative.svg")
+
+--- image-exif-rotation ---
+#let data = read("/assets/images/f2t.jpg", encoding: none)
+
+#let rotations = range(1, 9)
+#let rotated(v) = image(data.slice(0, 49) + bytes((v,)) + data.slice(50), width: 10pt)
+
+#set page(width: auto)
+#table(
+  columns: rotations.len(),
+  ..rotations.map(v => raw(str(v), lang: "typc")),
+  ..rotations.map(rotated)
+)

commit 8fccd617b21b4ba7e987cbc0afd4ecaade559570

Fix exif rotation regression
Introduced in https://github.com/typst/typst/pull/6102/

crates/typst-library/src/visualize/image/raster.rs
@@ -346,12 +346,12 @@ fn apply_rotation(image: &mut DynamicImage, rotation: u32) {
             ops::flip_horizontal_in_place(image);
             *image = image.rotate270();
         }
-        6 => *image = image.rotate270(),
+        6 => *image = image.rotate90(),
         7 => {
             ops::flip_horizontal_in_place(image);
             *image = image.rotate90();
         }
-        8 => *image = image.rotate90(),
+        8 => *image = image.rotate270(),
         _ => {}
     }
 }

crates/typst-pdf/src/image.rs
@@ -248,9 +248,9 @@ fn exif_transform(image: &RasterImage, size: Size) -> (Transform, Size) {
         Some(3) => no_flipping(true, true),
         Some(4) => no_flipping(false, true),
         Some(5) => with_flipping(false, false),
-        Some(6) => with_flipping(true, false),
+        Some(6) => with_flipping(false, true),
         Some(7) => with_flipping(true, true),
-        Some(8) => with_flipping(false, true),
+        Some(8) => with_flipping(true, false),
         _ => no_flipping(false, false),
     }
 }