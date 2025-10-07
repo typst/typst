78c568237290113c891927518a75571b131eecf7
Laurenz <laurmaedje@gmail.com>
2025-10-07 14:40:47 +0200

    searched -> considered

--- crates/typst-cli/src/args.rs
+++ crates/typst-cli/src/args.rs
@@ -407,7 +407,7 @@ pub struct FontArgs {
     #[arg(long, env = "TYPST_IGNORE_SYSTEM_FONTS")]
     pub ignore_system_fonts: bool,
 
-    /// Ensures fonts embedded into Typst won't be searched.
+    /// Ensures fonts embedded into Typst won't be considered.
     #[cfg(feature = "embed-fonts")]
     #[arg(long, env = "TYPST_IGNORE_EMBEDDED_FONTS")]
     pub ignore_embedded_fonts: bool,
