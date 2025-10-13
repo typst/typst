5b782c29cc22d9f33e0b761c385f375c417a1a0a
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-10-13 16:47:03 +0800

    Fix HTML meta charset and add viewport meta tag

--- crates/typst-cli/src/server.rs
+++ crates/typst-cli/src/server.rs
@@ -184,7 +184,8 @@ const PLACEHOLDER_HTML: &str = "\
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset='utf-8'>
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
     <title>Waiting for {INPUT}</title>
     <style>
       body {
