97e38ac3043589249ac355ea5fb51e9bfac1ec10
Andrew Voynov <andrewvoynov.b@gmail.com>
2025-10-22 12:08:48 +0300

    Improve documentation for hide

--- crates/typst-library/src/layout/hide.rs
+++ crates/typst-library/src/layout/hide.rs
@@ -3,10 +3,13 @@ use crate::introspection::Tagged;
 
 /// Hides content without affecting layout.
 ///
-/// The `hide` function allows you to hide content while the layout still 'sees'
-/// it. This is useful to create whitespace that is exactly as large as some
+/// The `hide` function allows you to hide content while the layout still "sees"
+/// it. This is useful to create blank space that is exactly as large as some
 /// content. It may also be useful to redact content because its arguments are
-/// not included in the output.
+/// not included in the output, at least visually. However, there can be _some_
+/// traces of the hidden content such as bookmarked heading in the PDF Document
+/// Outline. Generally speaking, it shouldn't be relied upon for hiding
+/// sensitive information, as some content can be reverse engineered.
 ///
 /// # Example
 /// ```example
