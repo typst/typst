f4e9cbdb9669d159c0983f2ea36ca6e2550a1d25
Gabriel Araújo <gabriel140492@gmail.com>
2025-10-26 11:21:24 -0300

    fix fuzz testing

--- tests/fuzz/src/bin/paged.rs
+++ tests/fuzz/src/bin/paged.rs
@@ -9,8 +9,11 @@ fuzz_target!(|text: &str| {
     let world = FuzzWorld::new(text);
     if let Ok(document) = typst::compile::<PagedDocument>(&world).output {
         if let Some(page) = document.pages.first() {
-            std::hint::black_box(typst_render::render(page, 1.0));
-            std::hint::black_box(typst_svg::svg(page));
+            let opts = typst_render::Options { pixel_per_pt: 1.0, render_bleed: false };
+            std::hint::black_box(typst_render::render(page, opts));
+
+            let opts = typst_svg::Options { render_bleed: false };
+            std::hint::black_box(typst_svg::svg(page, opts));
         }
         _ = std::hint::black_box(typst_pdf::pdf(&document, &PdfOptions::default()));
     }
