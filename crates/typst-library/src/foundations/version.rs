dde7d84f28ac8b9906450603eee53cb58cd3f573
Andrew Voynov <37143421+Andrew15-5@users.noreply.github.com>
2025-10-30 12:00:02 +0300

    Document version type comparison (#7152)
    Co-authored-by: Malo <57839069+MDLC01@users.noreply.github.com>
    Co-authored-by: Laurenz <laurmaedje@gmail.com>

--- crates/typst-library/src/foundations/version.rs
+++ crates/typst-library/src/foundations/version.rs
@@ -65,12 +65,21 @@ impl Version {
     ///
     /// It can have any number of components (even zero).
     ///
-    /// ```example
+    /// ```example:"Constructing versions"
     /// #version() \
     /// #version(1) \
     /// #version(1, 2, 3, 4) \
     /// #version((1, 2, 3, 4)) \
     /// #version((1, 2), 3)
+    /// ```
+    ///
+    /// As a practical use case, this allows comparing the current version
+    /// ([`{sys.version}`]($version)) to a specific one.
+    ///
+    /// ```example:"Comparing with the current version"
+    /// Current version: #sys.version \
+    /// #(sys.version >= version(0, 14, 0)) \
+    /// #(version(3, 2, 0) > version(4, 1, 0))
     /// ```
     #[func(constructor)]
     pub fn construct(
