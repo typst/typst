0c732d98cb9d7b4164b9fcbc2c0b60211eb61c6c
Malo <57839069+MDLC01@users.noreply.github.com>
2025-11-01 18:46:11 +0100

    Implement `filter` and `map` for `arguments`

--- crates/typst-library/src/foundations/args.rs
+++ crates/typst-library/src/foundations/args.rs
@@ -1,12 +1,16 @@
 use std::fmt::{self, Debug, Formatter};
 use std::ops::Add;
+use std::slice;
 
+use comemo::Tracked;
 use ecow::{EcoString, EcoVec, eco_format, eco_vec};
 use typst_syntax::{Span, Spanned};
 
 use crate::diag::{At, SourceDiagnostic, SourceResult, StrResult, bail, error};
+use crate::engine::Engine;
 use crate::foundations::{
-    Array, Dict, FromValue, IntoValue, Repr, Str, Value, cast, func, repr, scope, ty,
+    Array, Context, Dict, FromValue, Func, IntoValue, Repr, Str, Value, cast, func, repr,
+    scope, ty,
 };
 
 /// Captured arguments to a function.
@@ -354,6 +358,64 @@ impl Args {
             .filter_map(|item| item.name.clone().map(|name| (name, item.value.v.clone())))
             .collect()
     }
+
+    /// Produces a new `arguments` with only the arguments for which the value
+    /// passes the test.
+    ///
+    /// ```example
+    /// #{
+    ///   arguments(-1, a: 0, b: 1, 2)
+    ///     .filter(v => v > 0)
+    /// }
+    /// ```
+    #[func]
+    pub fn filter(
+        self,
+        engine: &mut Engine,
+        context: Tracked<Context>,
+        /// The function to apply to each value. Must return a boolean.
+        test: Func,
+    ) -> SourceResult<Args> {
+        let mut run_test = |v: &Value| {
+            test.call(engine, context, [v.clone()])?
+                .cast::<bool>()
+                .at(test.span())
+        };
+        self.into_iter()
+            .filter_map(|arg| {
+                run_test(&arg.value.v).map(|b| b.then_some(arg)).transpose()
+            })
+            .collect()
+    }
+
+    /// Produces a new `arguments` by transforming each argument value with the
+    /// passed function.
+    ///
+    /// ```example
+    /// #{
+    ///   arguments(0, a: 1, 2)
+    ///     .map(v => v + 1)
+    /// }
+    /// ```
+    #[func]
+    pub fn map(
+        self,
+        engine: &mut Engine,
+        context: Tracked<Context>,
+        /// The function to apply to each value.
+        mapper: Func,
+    ) -> SourceResult<Args> {
+        self.into_iter()
+            .map(|arg| {
+                let mapped_value = mapper.call(engine, context, [arg.value.v])?;
+                Ok(Arg {
+                    span: Span::detached(),
+                    name: arg.name,
+                    value: Spanned::detached(mapped_value),
+                })
+            })
+            .collect()
+    }
 }
 
 impl Debug for Args {
@@ -387,6 +449,33 @@ impl Add for Args {
         self.items.extend(rhs.items);
         self.span = Span::detached();
         self
+    }
+}
+
+impl FromIterator<Arg> for Args {
+    fn from_iter<T: IntoIterator<Item = Arg>>(iter: T) -> Self {
+        Self {
+            span: Span::detached(),
+            items: iter.into_iter().collect(),
+        }
+    }
+}
+
+impl IntoIterator for Args {
+    type Item = Arg;
+    type IntoIter = <EcoVec<Arg> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a Args {
+    type Item = &'a Arg;
+    type IntoIter = slice::Iter<'a, Arg>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.iter()
     }
 }
 
