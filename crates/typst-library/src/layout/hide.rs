6b061187ed0790a0e3a368bf6e38a1feb54f5af3
Andrew Voynov <andrewvoynov.b@gmail.com>
2025-11-07 17:31:01 +0300

    Use more natural and formal wording
    The "useful to" expression is used as "useful to someone/something", a
    correct/natural expression would be "useful for doing something".
    
    In the Glossary of PDF terms, the term is "Document Outline".
    https://pdfa.org/glossary-of-pdf-terms

--- crates/typst-library/src/layout/hide.rs
+++ crates/typst-library/src/layout/hide.rs
@@ -4,7 +4,7 @@ use crate::introspection::Tagged;
 /// Hides content without affecting layout.
 ///
 /// The `hide` function allows you to hide content while the layout still "sees"
-/// it. This is useful to create blank space that is exactly as large as some
+/// it. This is useful for creating blank space that is exactly as large as some
 /// content.
 ///
 /// # Example
@@ -14,10 +14,10 @@ use crate::introspection::Tagged;
 /// ```
 ///
 /// # Redaction
-/// This function may also be useful to redact content as its arguments are
+/// This function may also be useful for redacting content as its arguments are
 /// neither present visually nor accessible to Assistive Technology. That said,
 /// there can be _some_ traces of the hidden content (such as a bookmarked
-/// heading in the PDF document outline).
+/// heading in the PDF's Document Outline).
 ///
 /// Note that, depending on the circumstances, it may be possible for content to
 /// be reverse engineered based on its size in the layout. We thus do not
