d84cbae88f27a16cebd22e6f59566430bab473f1
Andreas Hartmann <hartan@7x.de>
2025-11-08 16:27:38 +0100

    feat(library): Obtain Typst version from global version definition
    rather than manually reading out (crate-specific) environment variables.
    The latter approach may (in theory) cause different outputs than the
    former.

--- crates/typst-library/src/foundations/sys.rs
+++ crates/typst-library/src/foundations/sys.rs
@@ -5,14 +5,7 @@ use crate::foundations::{Dict, Module, Scope, Version};
 /// A module with system-related things.
 pub fn module(inputs: Dict) -> Module {
     let mut scope = Scope::deduplicating();
-    scope.define(
-        "version",
-        Version::from_iter([
-            env!("CARGO_PKG_VERSION_MAJOR").parse::<u32>().unwrap(),
-            env!("CARGO_PKG_VERSION_MINOR").parse::<u32>().unwrap(),
-            env!("CARGO_PKG_VERSION_PATCH").parse::<u32>().unwrap(),
-        ]),
-    );
+    scope.define("version", Version::from(typst_syntax::TypstVersion::new()));
     scope.define("inputs", inputs);
     Module::new("sys", scope)
 }
