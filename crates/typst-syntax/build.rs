e6ba52200cf6c304e821f9a0004582ff900586ec
Andreas Hartmann <hartan@7x.de>
2025-11-16 17:49:37 +0100

    build: Don't forcefully set `TYPST_VERSION`
    but instead have the crate rebuilt when `CARGO_PKG_VERSION` is changed.

--- crates/typst-syntax/build.rs
+++ crates/typst-syntax/build.rs
@@ -1,7 +1,4 @@
 fn main() {
     println!("cargo:rerun-if-env-changed=TYPST_VERSION");
-
-    if option_env!("TYPST_VERSION").is_none() {
-        println!("cargo:rustc-env=TYPST_VERSION={}", env!("CARGO_PKG_VERSION"));
-    }
+    println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
 }
