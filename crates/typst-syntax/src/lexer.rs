ab3ff542bf2379766b6398f76bb5554ab75e4db4
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-11-16 06:33:23 +0000

    style

--- crates/typst-syntax/src/lexer.rs
+++ crates/typst-syntax/src/lexer.rs
@@ -1,4 +1,5 @@
 use ecow::{EcoString, eco_format};
+use std::num::IntErrorKind;
 use typst_utils::default_math_class;
 use unicode_ident::{is_xid_continue, is_xid_start};
 use unicode_math_class::MathClass;
@@ -851,19 +852,21 @@ impl Lexer<'_> {
         let number = self.s.from(start);
         let suffix = self.s.eat_while(|c: char| c.is_ascii_alphanumeric() || c == '%');
 
+        // Parse large integer literals as floats
+        if base == 10
+            && !is_float
+            && let Err(e) = i64::from_str_radix(number, base)
+            && matches!(e.kind(), IntErrorKind::PosOverflow | IntErrorKind::NegOverflow)
+            && number.parse::<f64>().is_ok()
+        {
+            is_float = true;
+        }
+
         let mut suffix_result = match suffix {
             "" => Ok(None),
             "pt" | "mm" | "cm" | "in" | "deg" | "rad" | "em" | "fr" | "%" => Ok(Some(())),
             _ => Err(eco_format!("invalid number suffix: {suffix}")),
         };
-
-        if base == 10
-            && !is_float
-            && i64::from_str_radix(number, base).is_err()
-            && number.parse::<f64>().is_ok()
-        {
-            is_float = true;
-        }
 
         let number_result = if is_float && number.parse::<f64>().is_err() {
             // The only invalid case should be when a float lacks digits after
