06cc2e6837b94e0bac1e9af7b123dcc81468a98e
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-01-16 17:04:25 +0100

    Ensure diagnostics are emitted in all test stage branches

--- tests/src/run.rs
+++ tests/src/run.rs
@@ -91,10 +91,8 @@ impl<'a> Runner<'a> {
             log!(into: self.result.infos, "tree: {:#?}", self.test.source.root());
         }
 
-        // Only compile paged document when the paged target is specified or
-        // implied. If so, run tests for all paged outputs unless excluded by
-        // the `--stages` flag. The test attribute still control which
-        // reference outputs are saved and compared though.
+        // Only compile paged document when the paged target is explicitly
+        // specified or required by paged outputs.
         if ARGS.should_run(self.test.attrs.stages & TestStages::PAGED_STAGES) {
             let mut doc = self.compile::<PagedDocument>(TestTarget::Paged);
 
@@ -117,7 +115,7 @@ impl<'a> Runner<'a> {
             }
             if ARGS.should_run(self.test.attrs.stages & TestStages::PDF_STAGES) {
                 let pdf = self.run_hash_test::<output::Pdf>(doc.as_ref());
-                if ARGS.should_run(TestStages::PDFTAGS) {
+                if ARGS.should_run(self.test.attrs.stages & TestStages::PDFTAGS) {
                     self.run_file_test::<output::Pdftags>(pdf.as_ref());
                 }
             }
@@ -148,35 +146,41 @@ impl<'a> Runner<'a> {
 
     /// Handle notes that weren't handled before.
     fn handle_not_emitted(&mut self) {
-        let mut first = true;
         for (note, &seen) in self.test.notes.iter().zip(&self.seen) {
-            let mut missing_stages = TestStages::empty();
-            let required_stages = ARGS.stages() & self.test.attrs.stages;
-
-            // If a test stage requiring the paged target has been specified,
-            // require the annotated diagnostics to be present in any paged
-            // stage. For example if `pdftags` is specified and an error is
-            // emitted in the pdf stage, that is ok.
-            if !(required_stages & TestStages::PAGED_STAGES).is_empty()
-                && (seen & TestStages::PAGED_STAGES).is_empty()
-            {
-                missing_stages |= TestStages::PAGED;
-            }
-            if !(required_stages & TestStages::HTML).is_empty()
-                && (seen & TestStages::HTML).is_empty()
-            {
-                missing_stages |= TestStages::HTML;
-            }
-
-            if missing_stages.is_empty() {
+            let possible = self.test.attrs.stages & ARGS.stages();
+            if seen.is_empty() && !possible.is_empty() {
+                log!(self, "not emitted");
+                let note_range = self.format_range(note.file, &note.range);
+                log!(
+                    self,
+                    "  {}: {note_range} {} ({})",
+                    note.kind,
+                    note.message,
+                    note.pos
+                );
                 continue;
             }
-            let note_range = self.format_range(note.file, &note.range);
-            if first {
-                log!(self, "not emitted [{missing_stages}]");
-                first = false;
+
+            // Figure out if the diagnostic is only emitted in a specific in a
+            // specific output/target that isn't hit by all possible branches of
+            // the stage tree.
+            // See the doc comment on `TestStages` for an overview.
+            let attr_stages = self.test.attrs.stages & ARGS.stages();
+            let full_branch_coverage =
+                attr_stages.iter().all(|s| s.with_required().intersects(seen));
+            if full_branch_coverage {
+                continue;
             }
-            log!(self, "  {}: {note_range} {} ({})", note.kind, note.message, note.pos,);
+
+            let siblings = seen.with_siblings() & self.test.attrs.stages & ARGS.stages();
+            log!(
+                self,
+                "only emitted in [{seen}] but expected in [{siblings}], \
+                 consider narrowing the test attributes",
+            );
+
+            let note_range = self.format_range(note.file, &note.range);
+            log!(self, "  {}: {note_range} {} ({})", note.kind, note.message, note.pos);
         }
     }
 
