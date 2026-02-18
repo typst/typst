a22f2735a71e7b9a3d5d879bd7eff12f5605a5d6
wmedrano <will@wmedrano.dev>
2026-02-17 18:08:30 -0800

    Update resvg deps

--- crates/typst-library/src/visualize/image/raster.rs
+++ crates/typst-library/src/visualize/image/raster.rs
@@ -421,21 +421,7 @@ fn jpeg_dpi(data: &[u8]) -> Option<f64> {
 /// Tries to extract the DPI from raw PNG data.
 fn png_dpi(mut data: &[u8]) -> Option<f64> {
     let mut decoder = png::StreamingDecoder::new();
-    let dims = loop {
-        let (consumed, event) = decoder.update(data, &mut Vec::new()).ok()?;
-        match event {
-            png::Decoded::PixelDimensions(dims) => break dims,
-            // Bail as soon as there is anything data-like.
-            png::Decoded::ChunkBegin(_, png::chunk::IDAT)
-            | png::Decoded::ImageData
-            | png::Decoded::ImageEnd => return None,
-            _ => {}
-        }
-        data = data.get(consumed..)?;
-        if consumed == 0 {
-            return None;
-        }
-    };
+    let dims = todo!();
 
     let dpu = dims.xppu.max(dims.yppu) as f64;
     match dims.unit {
