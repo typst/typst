820983bd12cd4170584e10292a5be2203090b2e5
Ian Wrzesinski <ian@isuffix.com>
2026-03-03 16:00:27 -0500

    Fix tooltips for math function calls and add tooltip tests

--- crates/typst-eval/src/call.rs
+++ crates/typst-eval/src/call.rs
@@ -93,6 +93,9 @@ fn eval_math_call(vm: &mut Vm, math_call: ast::MathCall) -> SourceResult<Value> 
     let math_call_result = match callee {
         ast::MathCallee::MathIdent(ident) => {
             let callee_value = ident.eval(vm)?;
+            // We need to call `trace_at` for the callee manually because we did
+            // not evaluate via `ast::Expr::eval()`.
+            vm.trace_at(ident.span(), &callee_value);
             match callee_value.clone().cast::<Func>() {
                 Ok(func) => FieldCallee::Func(func),
                 Err(err) => FieldCallee::NonFunc(callee_value, err),
