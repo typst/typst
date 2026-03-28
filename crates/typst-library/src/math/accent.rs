a39904b8e346e665d2fd9d9c1fb0bcffd0b02e36
TheJanzap <16736682+TheJanzap@users.noreply.github.com>
2026-03-28 22:20:44 +0100

    docs(accent): Change sentence

--- crates/typst-library/src/math/accent.rs
+++ crates/typst-library/src/math/accent.rs
@@ -22,7 +22,7 @@ pub const ACCENT_SHORT_FALL: Em = Em::new(0.5);
 /// Attaches an accent to a base.
 ///
 /// In math mode, each accent name is its own function and can be used
-/// without `accent`. 
+/// directly without `accent`.
 ///
 /// # Example
 /// ```example
