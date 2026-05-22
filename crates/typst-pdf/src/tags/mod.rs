33e2a2830d360660f0533ab50477046c01da90dc
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-05-22 12:27:35 +0200

    Fix tagged PDF bounding boxes for shapes with strokes

--- crates/typst-pdf/src/tags/mod.rs
+++ crates/typst-pdf/src/tags/mod.rs
@@ -242,7 +242,7 @@ pub fn shape<'a, 'b>(
         return TagHandle { surface, started: false };
     }
 
-    update_bbox(gc, fc, || shape.geometry.bbox(None));
+    update_bbox(gc, fc, || shape.bbox(true));
 
     if gc.tags.tree.parent_artifact().is_some() {
         return TagHandle { surface, started: false };
