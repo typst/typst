63a1ba677f46c546e459d1e0f5dc5607b169e008
Jassiel Ovando <jassielovando@protonmail.com>
2026-08-11 15:48:52 -0400

    Add document information in fuzz tests

--- tests/fuzz/src/bin/paged.rs
+++ tests/fuzz/src/bin/paged.rs
@@ -1,6 +1,7 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use typst::model::Document;
 use typst_fuzz::FuzzWorld;
 use typst_layout::PagedDocument;
 use typst_pdf::PdfOptions;
@@ -12,7 +13,11 @@ fuzz_target!(|text: &str| {
     if let Ok(document) = typst::compile::<PagedDocument>(&world).output {
         if let Some(page) = document.pages().first() {
             std::hint::black_box(typst_render::render(page, &RenderOptions::default()));
-            std::hint::black_box(typst_svg::svg(page, &SvgOptions::default()));
+            std::hint::black_box(typst_svg::svg(
+                page,
+                document.info(),
+                &SvgOptions::default(),
+            ));
         }
         _ = std::hint::black_box(typst_pdf::pdf(&document, &PdfOptions::default()));
     }
