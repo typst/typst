d62c0ed38b3871dda369695c8fdfbd0344485bca
rinmyo <satanyalin@gmail.com>
2026-09-09 09:14:11 +0900

    make CI pass

--- crates/typst-svg/src/lib.rs
+++ crates/typst-svg/src/lib.rs
@@ -77,7 +77,7 @@ pub fn svg_in_bundle(
 
 /// Export a frame into an SVG suitable for embedding into HTML.
 ///
-/// Glyphs, clip paths, gradients and tilings are collected into `<defs>` 
+/// Glyphs, clip paths, gradients and tilings are collected into `<defs>`
 /// rather than written into the frame.
 ///
 /// Takes additional `anchor` locations that will be serialized as linkable
