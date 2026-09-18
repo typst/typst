842333ef3b7269fbcf561bce27f0ba9bb8664d7e
Valentin Trophime <valentin.trophime@inria.fr>
2026-09-18 17:02:44 +0200

    Add font name to warning about font not designed for math

--- crates/typst-layout/src/math/mod.rs
+++ crates/typst-layout/src/math/mod.rs
@@ -641,7 +641,8 @@ fn warn_non_math_font(font: &Font, engine: &mut Engine, span: Span) {
     if !font.info().flags.contains(FontFlags::MATH) {
         engine.sink.warn(warning!(
             span,
-            "current font is not designed for math";
+            "current font ({}) is not designed for math",
+            font.post_script_name().unwrap_or_default();
             hint: "rendering may be poor";
         ));
     }
