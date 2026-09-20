a47267342a2c873d49d6b9bf7b926b045e2f943a
Valentin Trophime <valentin.trophime@inria.fr>
2026-09-20 12:04:24 +0200

    apply suggestions from @laurmaedje (family name instead of post_script)

--- crates/typst-layout/src/math/mod.rs
+++ crates/typst-layout/src/math/mod.rs
@@ -641,8 +641,8 @@ fn warn_non_math_font(font: &Font, engine: &mut Engine, span: Span) {
     if !font.info().flags.contains(FontFlags::MATH) {
         engine.sink.warn(warning!(
             span,
-            "current font ({}) is not designed for math",
-            font.post_script_name().unwrap_or_default();
+            "font family is not designed for math: {}",
+            font.info().family;
             hint: "rendering may be poor";
         ));
     }
